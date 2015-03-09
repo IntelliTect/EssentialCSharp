26ab90730dea125f57e31604b7d63865dd9d45a6
Mark Michaelis <Mark@IntelliTect.com>
2015-03-09 05:19:33 -0700

    Change the base used for conversion in the test to binary.

--- EssentialCSharp/Chapter03.Tests/Listing03.39.StringRepresentationOfBinary.Tests.cs
+++ EssentialCSharp/Chapter03.Tests/Listing03.39.StringRepresentationOfBinary.Tests.cs
@@ -10,7 +10,7 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter03.Listing03_39.Tests
         {
             int integer = new System.Random().Next();
             string view =
-                $@"Enter an integer: <<{integer}>>{System.Convert.ToString(integer, 10).PadLeft(64,'0')}";
+                $@"Enter an integer: <<{integer}>>{System.Convert.ToString(integer, 2).PadLeft(64,'0')}";
 
             IntelliTect.ConsoleView.Tester.Test(view,
             () =>
