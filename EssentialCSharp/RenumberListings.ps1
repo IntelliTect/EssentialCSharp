cb126f5841d0144ba7a14601c11d1f809361f4d4
Mark Michaelis <Mark@IntelliTect.com>
2015-07-06 06:00:21 -0700

    Incremented the listings for Chapter 02 from renaming 12A to 13...

--- EssentialCSharp/RenumberListings.ps1
+++ EssentialCSharp/RenumberListings.ps1
@@ -36,8 +36,8 @@ Get-Item .\Chapter13\Listing13* | IncrementListing.ps1 13 06
 [CmdletBinding(SupportsShouldProcess=$True)]
 param(
     [Parameter(Mandatory,ValueFromPipeline,ParameterSetName="Files")][string[]] $Files,
-    [Parameter(Mandatory,ParameterSetName="ChapterNumber")][int] $Chapter,
-    [Parameter()][string] $StartWithListing,
+    [Parameter(Mandatory,ParameterSetName="ChapterNumber")][int] $ChapterNumber,
+    [Parameter()][int] $StartWithListing,
     [Parameter(Mandatory)][ValidateSet('Decrement','Increment')][string] $Direction
 )
 
@@ -51,8 +51,9 @@ PROCESS {
     # $PSScriptRoot is not populated when you hover over it is ISE.
     # StrictMode has to be set after parameters if you are using CmdletBinding (perhaps regardless).
 
-    $StartWithListing = $StartWithListing.PadLeft(2, '0')
-
+    $StartWithListing = $StartWithListing.ToString().PadLeft(2, '0')
+    [string]$Chapter = $ChapterNumber.ToString().PadLeft(2, '0')
+    
     switch($PsCmdlet.ParameterSetName)
     {
         "ChapterNumber" {
@@ -73,18 +74,33 @@ PROCESS {
             $difference = -1
             break
         } 
-        "Decrement"  { 
+        "Increment"  { 
             $files | Sort -Descending
             $difference = 1
             break
         } 
     }
 
+    $proj = [XML](Get-Content "$PSScriptRoot\Chapter$Chapter\Chapter$Chapter.csproj")
+
     foreach( $file in $Files) {
         $file -match $listingNameRegEx | %{  
-            $command = "git.exe mv `"$PSScriptRoot\Chapter$Chapter\$($matches[0])`" `"$PSScriptRoot\Chapter$Chapter\Listing$($matches.chapter).$(([int]::Parse($matches.listing) + $difference).ToString().PadLeft(2, '0'))$($matches.suffix)`" $(if($PSBoundParameters['Verbose']) {`"-v`"}) $(if($PSBoundParameters['WhatIf']) {`"--dry-run`"})"
+            if($file.FullName -ne "$PSScriptRoot\Chapter$Chapter\$($matches[0])") { throw '$file unexpectedly doesn''t match $PSScriptRoot\Chapter$Chapter\$($matches[0])' }
+            $oldFileName = $file.FullName
+            $newFileName = "$PSScriptRoot\Chapter$Chapter\Listing$($matches.chapter).$(([int]::Parse($matches.listing) + $difference).ToString().PadLeft(2, '0'))$($matches.suffix)"
+            $command = "git.exe mv `"$PSScriptRoot\Chapter$Chapter\$($matches[0])`" $newFileName $(if($PSBoundParameters['Verbose']) {`"-v`"})" # The following is not needed as it is handled by "$PSCmdlet.ShouldProcess": -What $(if($PSBoundParameters['WhatIf']) {`"--dry-run`"})"
             if ($PSCmdlet.ShouldProcess("`tExecuting: $command", "`tExecute git.exe Rename: $command", "Executing Git.exe mv")) {
                 Invoke-Expression "$command"; 
+            }
+            if ($PSCmdlet.ShouldProcess(
+                "`tUpdating Chapter$Chapter\Chapter$Chapter.csproj - Changing to compile '$([IO.Path]::GetFileName($newFileName))' rather than '$($file.Name)'",
+                "`tUpdating Chapter$Chapter\Chapter$Chapter.csproj - Changing to compile '$([IO.Path]::GetFileName($newFileName))' rather than '$($file.Name)'",
+                "Updating Chapter$Chapter\Chapter$Chapter.csproj:"
+                )) {
+                $proj.Project.ItemGroup.SelectNodes('//*[local-name()="Compile"]') | 
+                    ?{ $_.Include -eq $file.Name }  | #Change to use XPath to find element as this makes no check that the element exists.
+                        %{ $_.Include = [IO.Path]::GetFileName($newFileName) }
+                $proj.Save("$PSScriptRoot\Chapter$Chapter\Chapter$Chapter.csproj") # Saving as each file is renamed rather than all at the end in case we error out in the middle.
             }
         }
     }
