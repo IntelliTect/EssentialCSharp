dbb6f4a96b0b581f4b5e18d7cb93bc51104c2d35
Jason <jason.peterson@intellitect.com>
2017-02-10 09:58:42 -0800

    Update a test to pass on Ubuntu 16.04

--- src/Chapter15.Tests/Listing15.15.Tests.cs
+++ src/Chapter15.Tests/Listing15.15.Tests.cs
@@ -20,8 +20,8 @@ Prepend, Range, Repeat, Reverse, Select, SelectMany, SequenceEqual,
 Single, SingleOrDefault, Skip, SkipWhile, Sum, Take, TakeWhile, ThenBy, 
 ThenByDescending, ToArray, ToDictionary, ToList, ToLookup, Union, Where, Zip, ";
 
-            expected = expected.Replace("\r\n", "");
-
+            expected = expected.Replace("\r\n", string.Empty);
+            expected = expected.Replace("\n", string.Empty);
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(expected, Program.ChapterMain);
         }
     }
