69989306ca173350ae9cf7bdb9f982ee1abcef31
Mark Michaelis <mark@IntelliTect.com>
2017-03-26 21:28:53 -0700

    Added examples for binary numeric literals; Added digit separator example.

---
+++ src/Chapter02/Listing02.03B.UsingTheDigitSeparator.cs
@@ -0,0 +1,10 @@
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_03B
+{
+    public class Program
+    {
+        public static void ChapterMain()
+        {
+            System.Console.WriteLine(9_814_072_356M);
+        }
+    }
+}
---
+++ src/Chapter02/Listing02.05B.UsingTheDigitSeparator.cs
@@ -0,0 +1,11 @@
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_05B
+{
+    public class Program
+    {
+        public static void ChapterMain()
+        {
+            // Display the value 42 using a binary literal. 
+            System.Console.WriteLine(0b101010);
+        }
+    }
+}
