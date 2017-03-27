3ea4cb651bc8dc77266155ef7b09fb435492ed35
Mark Michaelis <mark@IntelliTect.com>
2017-03-26 21:28:53 -0700

    Added examples for binary numeric literals; Added digit separator example.

---
+++ src/Chapter02.Tests/Listing02.06.Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_06.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void Main_WriteDecimalAsHexidecimalUsingFormatSpecifier()
+        {
+            const string expected = "0x2A";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Program.ChapterMain);
+        }
+    }
+}
