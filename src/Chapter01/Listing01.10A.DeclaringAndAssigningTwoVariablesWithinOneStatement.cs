559685ffe5ea8fa07e5212851ee3f5538afbcd61
Mark.Michaelis <mark@michaelis.net>
2017-03-29 13:39:04 -0700

    Added tuple examples.

---
+++ src/Chapter01/Listing01.10A.DeclaringAndAssigningTwoVariablesWithinOneStatement.cs
@@ -0,0 +1,10 @@
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter01.Listing01_10A
+{
+    public class HelloWorld
+    {
+        public static void ChapterMain()
+        {
+            (string firstName, string lastName) = ("Inigo", "Montoya");
+        }
+    }
+}
---
+++ src/Chapter02.Tests/Listing02.30B.Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_30B.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void Main_WorkingWithTuples()
+        {
+            string expected = @"Sherlock Holme's address was:
+221B Baker Street
+Marylebone, London  NW1 6XE
+England";
+            expected += $"{System.Environment.NewLine + expected}";
+            expected += $"{System.Environment.NewLine + expected}";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Program.ChapterMain);
+
+        }
+    }
+}
