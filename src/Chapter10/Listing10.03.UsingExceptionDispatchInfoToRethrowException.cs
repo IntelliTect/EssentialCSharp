624492a518b0462659bed7b16be960c781fe74fb
Mark Michaelis <mark@IntelliTect.com>
2017-04-30 16:26:27 -0700

    git update submodule for TestTools

--- src/Chapter10/Listing10.03.UsingExceptionDispatchInfoToRethrowException.cs
+++ src/Chapter10/Listing10.03.UsingExceptionDispatchInfoToRethrowException.cs
@@ -1,38 +1,86 @@
 ﻿namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter10.Listing10_03
 {
+#pragma warning disable 0168 // Disabled warning for unused variables for elucidation
     using System;
+    using System.IO;
+    using System.Linq;
+    using System.Net;
+    using System.Runtime.ExceptionServices;
+    using System.Threading.Tasks;
 
     public sealed class Program
     {
-        public static void ChapterMain()
+        public static void ChapterMain(string[] args)
         {
+            string url = "http://www.IntelliTect.com";
+            if (args.Length > 0)
+            {
+                url = args[0];
+            }
+
+            Console.Write(url);
+            Task task = WriteWebRequestSizeAsync(url);
             try
             {
-                // ...
-                throw new InvalidOperationException(
-                    "Arbitrary exception");
-                // ...
+                while (!task.Wait(100))
+                {
+                    Console.Write(".");
+                }
             }
-            catch(NullReferenceException exception)
+            catch (AggregateException exception)
             {
-                // Handle NullReferenceException
+                exception = exception.Flatten();
+                ExceptionDispatchInfo.Capture(
+                    exception.InnerException).Throw();
             }
-            catch(ArgumentException exception)
+
+        }
+
+
+        private static Task WriteWebRequestSizeAsync(
+            string url)
+        {
+            StreamReader reader = null;
+            WebRequest webRequest =
+                 WebRequest.Create(url);
+
+            Task task =
+                webRequest.GetResponseAsync()
+            .ContinueWith(antecedent =>
             {
-                // Handle ArgumentException
-            }
-            catch(InvalidOperationException exception)
+                WebResponse response =
+                   antecedent.Result;
+
+                reader =
+                    new StreamReader(
+                        response.GetResponseStream());
+                return reader.ReadToEndAsync();
+            })
+            .Unwrap()
+            .ContinueWith(antecedent =>
             {
-                // Handle ApplicationException
-            }
-            catch(Exception exception)
-            {
-                // Handle Exception
-            }
-            catch
-            {
-                // Any unhandled exception
-            }
+                if (reader != null)
+                    reader.Dispose();
+                string text = antecedent.Result;
+                Console.WriteLine(
+                    FormatBytes(text.Length));
+            });
+
+            return task;
+        }
+
+        static public string FormatBytes(long bytes)
+        {
+            string[] magnitudes =
+                new string[] { "GB", "MB", "KB", "Bytes" };
+            long max =
+                (long)Math.Pow(1024, magnitudes.Length);
+
+            return string.Format("{1:##.##} {0}",
+                magnitudes.FirstOrDefault(
+                    magnitude =>
+                        bytes > (max /= 1024)) ?? "0 Bytes",
+                    (decimal)bytes / (decimal)max).Trim();
         }
     }
 }
