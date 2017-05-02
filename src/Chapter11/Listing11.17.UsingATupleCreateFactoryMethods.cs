d6f04bd1f88e365aafade140c662c98f4643cc7a
Mark Michaelis <mark@IntelliTect.com>
2017-05-02 04:49:45 -0700

    Switched to use System.ValueTuple and demonstrate the difference in syntax with C# 7.0.

--- src/Chapter11/Listing11.17.UsingATupleCreateFactoryMethods.cs
+++ src/Chapter11/Listing11.17.UsingATupleCreateFactoryMethods.cs
@@ -1,5 +1,7 @@
 ﻿namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter11.Listing11_17
 {
+#pragma warning disable 0219 // Disabled warning for assigned but never 
+                             // used variables for elucidation
     using System;
     using Chapter07.Listing07_11;
 
@@ -7,13 +9,19 @@
     {
         public static void ChapterMain()
         {
-            Tuple<string, Contact> keyValuePair;
+#if !PRECSHARP7
+            (string, Contact) keyValuePair;
             keyValuePair =
-                Tuple.Create(
+                ("555-55-5555", new Contact("Inigo Montoya"));
+#elif CSHARP6
+            ValueTuple<string, Contact> keyValuePair;
+            keyValuePair =
+                new ValueTuple<string, Contact>(
                     "555-55-5555", new Contact("Inigo Montoya"));
             keyValuePair =
-                new Tuple<string, Contact>(
+                ValueTuple.Create(
                     "555-55-5555", new Contact("Inigo Montoya"));
+#endif // !PRECSHARP7
         }
     }
 }
