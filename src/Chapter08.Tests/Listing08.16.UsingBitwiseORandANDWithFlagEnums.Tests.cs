fded2352119ddcbd2fe70054b2079cc6c78463e4
Mark Michaelis <mark@IntelliTect.com>
2017-06-18 09:38:10 -0700

    Added Enum.HasFlag() method call; Added test project for Chapter 08.

---
+++ src/Chapter08.Tests/Listing08.16.UsingBitwiseORandANDWithFlagEnums.Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Reflection;
+using System.IO;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter08.Listing08_16.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void Main_ExpectHiddentAndReadOnlyFlags()
+        {
+            Directory.SetCurrentDirectory(AppContext.BaseDirectory);
+            const string expected =
+                @"Hidden | ReadOnly = 3";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Program.ChapterMain);
+            var text = nameof(AddisonWesley.Michaelis.EssentialCSharp.Chapter08.Listing08_16.Program.ChapterMain);
+        }
+    }
+}
