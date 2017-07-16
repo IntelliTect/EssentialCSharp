8f5c01651a96f9f397291922887fefccd5dc9d92
Mark Michaelis <mark@IntelliTect.com>
2017-07-16 12:22:43 -0700

    Moved Listing14.16A to Listing14.30 including the test file.

--- src/Chapter14/Listing14.30.ProjectionToAnAnonymousType.cs
+++ src/Chapter14/Listing14.30.ProjectionToAnAnonymousType.cs
@@ -1,4 +1,4 @@
-﻿namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter14.Listing14_16.XX
+﻿namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter14.Listing14_30
 {
     using System;
     using System.Collections.Generic;
