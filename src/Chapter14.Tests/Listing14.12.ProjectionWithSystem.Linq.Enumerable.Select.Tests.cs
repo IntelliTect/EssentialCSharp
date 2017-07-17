e20784d8b224189ee15446afba26bac7b3ae9452
Mark Michaelis <mark@IntelliTect.com>
2017-07-17 00:27:49 -0700

    Renumbered based on updated chapter for C# 7.0 which moved anonymous types to the end.

---
+++ src/Chapter14.Tests/Listing14.12.ProjectionWithSystem.Linq.Enumerable.Select.Tests.cs
@@ -0,0 +1,44 @@
+using IntelliTect.TestTools.Console;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter14.Listing14_15.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void Listing14_15_Test()
+        {
+            // Required due to defect in MSTest that has the current directory
+            // set to the MSTest executable directory, rather than the 
+            // assembly directory.
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(
+                typeof(Program).GetTypeInfo().Assembly.Location));
+
+            //string expectedPattern = $@"{ Directory.GetCurrentDirectory() }{Path.DirectorySeparatorChar}*";
+            string expectedPattern = $@"{ Directory.GetCurrentDirectory() }{ 
+                $"{Path.DirectorySeparatorChar}"}*";
+            int expectedItemCount = Directory.EnumerateFiles(
+                    Directory.GetCurrentDirectory(), "*").Count();
+            string output = ConsoleAssert.Execute(null, () =>
+            {
+                Program.ChapterMain();
+            });
+
+            IEnumerable<string> outputItems = output.Split(
+                new string[] { Environment.NewLine }, StringSplitOptions.None);
+
+            Assert.AreEqual<int>(
+                expectedItemCount, outputItems.Count());
+            foreach (string item in outputItems)
+            {
+                Assert.IsTrue(item.IsLike(expectedPattern, '?'));
+            }
+        }
+    }
+}
