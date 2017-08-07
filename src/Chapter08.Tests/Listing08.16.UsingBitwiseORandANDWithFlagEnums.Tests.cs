70e8676abec5ca861813f8d9a799059fdf9371a9
Mark Michaelis <mark@IntelliTect.com>
2017-08-07 03:58:35 -0700

    Fixed expectd output from test in Listing 08.16

--- src/Chapter08.Tests/Listing08.16.UsingBitwiseORandANDWithFlagEnums.Tests.cs
+++ src/Chapter08.Tests/Listing08.16.UsingBitwiseORandANDWithFlagEnums.Tests.cs
@@ -19,12 +19,14 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter08.Listing08_16.Tests
             {
                 // The only working file attribute on Linux is ReadOnly.
                 fileAttributes = FileAttributes.ReadOnly;
+                Assert.AreEqual<int>(1, (int)fileAttributes);
                 expected = $@"ReadOnly = {(int)fileAttributes}";
             }
             else
             {
                 fileAttributes = FileAttributes.Hidden | FileAttributes.ReadOnly;
-                expected = $@"Hidden | ReadOnly = {(int)fileAttributes}";
+                Assert.AreEqual<int>(3, (int)fileAttributes);
+                expected = $@"ReadOnly, Hidden = {(int)fileAttributes}";
             }
 
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(
