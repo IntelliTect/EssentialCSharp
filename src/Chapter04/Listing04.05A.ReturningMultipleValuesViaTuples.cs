43c227145e9a6ee39e860f1e8fcc526b297fb9c3
Mark Michaelis <mark@IntelliTect.com>
2017-09-09 17:07:39 -0700

    Added tests for returning multiple tuple values.

---
+++ src/Chapter04/Listing04.05A.ReturningMultipleValuesViaTuples.cs
@@ -0,0 +1,23 @@
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter04.Listing04_05A
+{
+    public class Program
+    {
+        static string GetUserInput(string prompt)
+        {
+            System.Console.Write(prompt);
+            return System.Console.ReadLine();
+        }
+        static (string First, string Last) GetName()
+        {
+            string firstName, lastName;
+            firstName = GetUserInput("Enter your first name: ");
+            lastName = GetUserInput("Enter your last name: ");
+            return (firstName, lastName);
+        }
+        static public void Main()
+        {
+            (string First, string Last) name = GetName();
+            System.Console.WriteLine($"Hello { name.First } { name.Last }!");
+        }
+    }
+}
