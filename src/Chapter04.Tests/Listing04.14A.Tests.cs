fac4125432ccc0c48c448da37edbd894577d8efc
Mark Michaelis <mark@IntelliTect.com>
2017-09-17 14:03:19 -0700

    Added support for reference return.

---
+++ src/Chapter04.Tests/Listing04.14A.Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter04.Listing04_14A.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void Main_InputInigoMontoya_WriteFullName()
+        {
+            string view =
+@"Enter your first name: <<Inigo
+>>Enter your last name: <<Montoya
+>>Hello Inigo Montoya!";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(view,
+            () =>
+            {
+                Program.Main();
+            });
+        }
+    }
+}
---
+++ src/Chapter05.Tests/Listing05.14A.Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter05.Listing05_14A.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void MainTest()
+        {
+            const string expected = @"image[[]*]=Red
+image[[]*]=Black";
+            IntelliTect.TestTools.Console.ConsoleAssert.ExpectLike(expected, () =>
+                {
+                    Program.Main();
+                });
+        }
+    }
+}
