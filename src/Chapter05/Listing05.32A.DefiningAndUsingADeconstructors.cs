95a6c7b4a40fc23b2d0b70ea2b1e0e2fdf019a8c
Mark Michaelis <mark@IntelliTect.com>
2017-09-17 03:15:30 -0700

    Added a deconstructor example.

---
+++ src/Chapter05/Listing05.32A.DefiningAndUsingADeconstructors.cs
@@ -0,0 +1,106 @@
+﻿namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter05.Listing05_32A
+{
+    using System;
+    using System.IO;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Employee employee;
+            employee = new Employee("Inigo", "Montoya");
+            employee.Salary = "Too Little";
+
+            System.Console.WriteLine(
+                "{0} {1}: {2}",
+                employee.FirstName,
+                employee.LastName,
+                employee.Salary);
+
+            (string firstName, string lastName, string salary) =
+                employee.Deconstruct();
+        }
+    }
+
+    class Employee
+    {
+        public Employee(string firstName, string lastName)
+        {
+            int id;
+            // Generate an employee ID...
+            id = 0; // id needs to be initialized for this example
+            // ...
+            Initialize(id, firstName, lastName);
+        }
+
+        public Employee(int id, string firstName, string lastName)
+        {
+            Initialize(id, firstName, lastName);
+        }
+
+        public Employee(int id)
+        {
+            string firstName;
+            string lastName;
+            Id = id;
+
+            // Look up employee data
+            firstName = string.Empty;
+            lastName = string.Empty;
+            // ...
+
+            Initialize(id, firstName, lastName);
+        }
+
+        private void Initialize(
+            int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public Deconstruct(out int id, out string firstName, out string lastName)
+        {
+            (int id, string firstName, string lastName, string salary) = 
+                (Id, FirstName, LastName, Salary)
+        }
+        // ...
+
+
+        public int Id { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Salary { get; set; } = "Not Enough";
+        public string Title { get; set; }
+        public Employee Manager { get; set; }
+
+        // Name property
+        public string Name
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+            set
+            {
+                // Split the assigned value into 
+                // first and last names.
+                string[] names;
+                names = value.Split(new char[] { ' ' });
+                if(names.Length == 2)
+                {
+                    FirstName = names[0];
+                    LastName = names[1];
+                }
+                else
+                {
+                    // Throw an exception if the full 
+                    // name was not assigned.
+                    throw new System.ArgumentException(
+                        $"Assigned value '{value}' is invalid");
+                }
+            }
+        }
+    }
+}
