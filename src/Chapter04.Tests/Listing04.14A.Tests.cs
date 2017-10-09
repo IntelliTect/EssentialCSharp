ede15a41ef911897f3f723d0dd07c2b4611c30af
Mark Michaelis <mark@IntelliTect.com>
2017-10-08 19:51:55 -0700

    Fixed defect in a test.

--- src/Chapter04.Tests/Listing04.14A.Tests.cs
+++ src/Chapter04.Tests/Listing04.14A.Tests.cs
@@ -8,10 +8,7 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter04.Listing04_14A.Tests
         [TestMethod]
         public void Main_InputInigoMontoya_WriteFullName()
         {
-            string view =
-@"Enter your first name: <<Inigo
->>Enter your last name: <<Montoya
->>Hello Inigo Montoya!";
+            string view = "Black";
 
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(view,
             () =>
