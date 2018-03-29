f6033ef21c21c38e37e8006dd580bcdc67bfb41d
Mark Michaelis <mark@IntelliTect.com>
2018-03-29 15:04:14 -0500

    Moved Test listing 04.03 to 05.03

--- src/Chapter05.Tests/Listing05.03.PassingAMethodReturnAsAParameterToAnotherMehtodCall.Tests.cs
+++ src/Chapter05.Tests/Listing05.03.PassingAMethodReturnAsAParameterToAnotherMehtodCall.Tests.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter04.Listing04_03.Tests
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter05.Listing05_03.Tests
 {
     [TestClass]
     public class ProgramTests
