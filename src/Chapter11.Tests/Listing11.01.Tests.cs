44f5f5ca70d54eb8213f14fc9b745825e704500f
Mark Michaelis <mark@IntelliTect.com>
2018-03-29 18:22:21 -0500

    Created test projects for Chapters 07 and 11.

---
+++ src/Chapter11.Tests/Listing11.01.Tests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter11.Listing11_01.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+
+        [TestMethod]
+        public void NeedsAName()
+        {
+            TextNumberParser.Parse("1");
+            throw new System.NotImplementedException();
+        }
+    }
+}
