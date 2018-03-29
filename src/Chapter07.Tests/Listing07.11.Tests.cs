482ff5b88fc0b2dde6c810ee465194cc8f860fa9
Mark Michaelis <mark@IntelliTect.com>
2018-03-29 18:22:21 -0500

    Created test projects for Chapters 07 and 11.

---
+++ src/Chapter07.Tests/Listing07.11.Tests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter07.Listing07_11.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+
+        [TestMethod]
+        public void NeedsAName()
+        {
+            Program.Main();
+            throw new System.NotImplementedException();
+        }
+    }
+}
