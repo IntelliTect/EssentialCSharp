0d067cd42f63aceb89a05bd7bfd1f64e009b28d2
Mark Michaelis <mark@IntelliTect.com>
2018-03-31 08:14:23 -0500

    Minor updated to namespace to indicate this has more than one example in it.

--- src/Chapter02/Listing02.01-2.06.SpecifyingLiteralValues.cs
+++ src/Chapter02/Listing02.01-2.06.SpecifyingLiteralValues.cs
@@ -1,4 +1,4 @@
-namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_01
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_01To06
 {
     public class Program
     {
