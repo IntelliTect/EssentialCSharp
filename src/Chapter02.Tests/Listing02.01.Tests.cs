5254f929ed2d8c0ecb0109e5c0ade6461e30c88d
Kyle Amonson <kyle.amonson@IntelliTect.com>
2018-10-08 11:56:11 -0700

    test fixes for v7.0 ch 02.01 - 02.07

--- src/Chapter02.Tests/Listing02.01.Tests.cs
+++ src/Chapter02.Tests/Listing02.01.Tests.cs
@@ -1,24 +1,44 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_01To06.Tests
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_01.Tests
 {
     [TestClass]
-    public class ProgramTests
+    public class LiteralValueTests
     {
         [TestMethod]
         public void Main_WriteNumbers()
         {
             const string expected =
 @"42
-1.618034
-1.61803398874989
-1.618033988749895
-6.023E+23
-42
-0x2A";
+1.618034";
 
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(
                 expected, Program.Main);
         }
     }
 }
+
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+//namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_01.Tests
+//{
+//    [TestClass]
+//    public class ProgramTests
+//    {
+//        [TestMethod]
+//        public void Main_WriteNumbers()
+//        {
+//            const string expected =
+//@"42
+//1.618034
+//1.61803398874989
+//1.618033988749895
+//6.023E+23
+//42
+//0x2A";
+
+//            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+//                expected, Program.Main);
+//        }
+//    }
+//}
