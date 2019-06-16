3631383acb46aafb1f3708e5194a7a8d5a6d2371
Mark Michaelis <Mark@IntelliTect.com>
2019-06-16 14:49:43 -0700

    Updated test 02.01 to support both netcoreapp2.0 and netcoreapp3.0. (There is currently a difference in rounding.)

--- src/Chapter02.Tests/Listing02.01.Tests.cs
+++ src/Chapter02.Tests/Listing02.01.Tests.cs
@@ -8,7 +8,7 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_01To06.Tes
         [TestMethod]
         public void Main_WriteNumbers()
         {
-            const string expected =
+            const string netCoreApp2expected =
 @"42
 1.618034
 1.61803398874989
@@ -17,8 +17,31 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_01To06.Tes
 42
 0x2A";
 
+            const string netCoreApp3expected =
+@"42
+1.618034
+1.618033988749895
+1.618033988749895
+6.023E+23
+42
+0x2A";
+            string expected;
+            string netCoreVersion = NetCore.GetNetCoreVersion();
+            if(netCoreVersion.StartsWith("2.1"))
+            {
+                expected = netCoreApp2expected;
+            }
+            else if(netCoreVersion.StartsWith("3"))
+            {
+                expected = netCoreApp3expected;
+            }
+            else
+            {
+                throw new System.Exception(".NET Core Version ({netCoreVersion}) not handled.");
+            }
+
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(
-                expected, Program.Main);
+                    expected, Program.Main);
         }
     }
 }
