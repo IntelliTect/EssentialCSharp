a0d98851aa7585717185e937b2be8fd27bb31143
Mark Michaelis <Mark@IntelliTect.com>
2019-06-16 17:25:10 -0700

    Updated test 02.09 to support both netcoreapp2.0 and netcoreapp3.0. (There is currently a difference in rounding.)

--- src/Chapter02.Tests/Listing02.09.Tests.cs
+++ src/Chapter02.Tests/Listing02.09.Tests.cs
@@ -8,12 +8,22 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_09.Tests
         [TestMethod]
         public void Main_WriteBooleanStatements()
         {
-            const string expected =
+            const string netCoreApp2expected =
 @"False: 1.61803398874989 == 1.61803398874989
 True: 1.61803398874989 == 1.61803398874989";
 
+            string expected = // netcoreapp3.0 and later
+@"True: 1.618033988749895 == 1.618033988749895
+True: 1.618033988749895 == 1.618033988749895";
+
+            string netCoreVersion = NetCore.GetNetCoreVersion();
+            if (string.Compare(netCoreVersion, "3")<0)
+            {
+                expected = netCoreApp2expected;
+            }
+
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(
-                expected, Program.Main);
+                    expected, Program.Main);
         }
     }
 }
