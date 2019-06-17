cf5afccff8e227749979223d3866f4b3a3ee3cf0
Mark Michaelis <Mark@IntelliTect.com>
2019-06-16 17:25:10 -0700

    Updated test 02.09 to support both netcoreapp2.0 and netcoreapp3.0. (There is currently a difference in rounding.)

---
+++ src/Shared/NetCore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+
+public class NetCore
+{
+    public static string GetNetCoreVersion()
+    {
+        var assembly = typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly;
+        var assemblyPath = assembly.CodeBase.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
+        int netCoreAppIndex = Array.IndexOf(assemblyPath, "Microsoft.NETCore.App");
+        if (netCoreAppIndex > 0 && netCoreAppIndex < assemblyPath.Length - 2)
+            return assemblyPath[netCoreAppIndex + 1];
+        return null;
+    }
+}
