42468ed197a550f202a50e0f6157d689c6ced832
Mark Michaelis <Mark@IntelliTect.com>
2019-06-16 21:58:29 -0700

    Updated to address differences in netcoreapp3.0 for listing 4.7

--- src/Chapter04.Tests/Listing04.07.UnexpectedInequality.Tests.cs
+++ src/Chapter04.Tests/Listing04.07.UnexpectedInequality.Tests.cs
@@ -8,8 +8,8 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter04.Listing04_07.Tests
         [TestMethod]
         public void MainTest()
         {
-            const string expected =
-                @"4.2 != 4.2000002861023
+            const string netCoreApp2expected =
+@"4.2 != 4.2000002861023
 4.2 != 4.20000028610229
 (float)4.2M != 4.2F
 4.20000028610229 != 4.2
@@ -17,10 +17,23 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter04.Listing04_07.Tests
 4.19999980926514 != 4.2
 4.2F != 4.2D";
 
-            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
-                expected, Program.Main);
+            string expected =
+                @"4.2 != 4.2000002861023
+4.2 != 4.200000286102295
+(float)4.2M != 4.2000003F
+4.200000286102295 != 4.2
+4.2000003F != 4.2D
+4.199999809265137 != 4.2
+4.2F != 4.2D";
 
-            // Removed 4.20000028610229 != 4.20000028610229 ... in .Net Core float conversions seem to be more accurate
+            string netCoreVersion = NetCore.GetNetCoreVersion();
+            if (string.Compare(netCoreVersion, "3") < 0)
+            {
+                expected = netCoreApp2expected;
+            }
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                    expected, Program.Main);
         }
     }
 }
