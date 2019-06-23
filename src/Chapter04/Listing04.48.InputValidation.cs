4cce13c538ccade4ab7cc75ef2c4556b6e9ae9be
Mark Michaelis <Mark@IntelliTect.com>
2019-06-23 16:05:57 -0700

    Updated to remove warning for 4.48. (Changes listing so not used for Essential C# 7.0.

--- src/Chapter04/Listing04.48.InputValidation.cs
+++ src/Chapter04/Listing04.48.InputValidation.cs
@@ -7,10 +7,8 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter04.Listing04_48
         {
             // ...
 
-            bool valid = false;
-
             // Check the current player's input
-            if ((input == "1") ||
+            if((input == "1") ||
                 (input == "2") ||
                 (input == "3") ||
                 (input == "4") ||
@@ -23,11 +21,12 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter04.Listing04_48
                 // Save/move as the player directed
                 // ...
 
-                valid = true;
             }
             else if((input == "") || (input == "quit"))
             {
-                valid = true;
+                // Retry or quite
+                // ...
+
             }
             else
             {
