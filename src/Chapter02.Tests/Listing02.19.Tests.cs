1338448db48640b8cc769c380f2a02dfc0e89da2
Mark Michaelis <Mark@IntelliTect.com>
2019-06-25 00:14:03 -0700

    Updated to use nullabilit modifier on value type as an example of null.

---
+++ src/Chapter02.Tests/Listing02.19.Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_19.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void Main_WriteOverflowExample()
+        {
+            const string expected = "The age is: ";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Program.Main);
+        }
+    }
+}
