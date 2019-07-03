c4ca2d5f35b84b5e3415f265834abb7468017845
Cameron Osborn <cameron0401@gmail.com>
2019-07-03 12:50:39 -0700

    Remove warnings from Chapter07

--- src/Chapter07/Listing07.06.PrivateMembersAreNotInherited.cs
+++ src/Chapter07/Listing07.06.PrivateMembersAreNotInherited.cs
@@ -1,5 +1,7 @@
 ﻿namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter07.Listing07_06
 {
+// In a properly implemented class we could use our Contact class
+#pragma warning disable CS0169
     public class PdaItem
     {
         private string _Name;
@@ -22,4 +24,5 @@
             //contact._Name = "Inigo Montoya";  //uncomment this line and it will not compile
         }
     }
+#pragma warning restore CS0169
 }
