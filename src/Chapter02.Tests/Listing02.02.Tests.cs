68cd75fb01882eac69b8c834badb53f1850fc1ef
Cameron Osborn <cameron0401@gmail.com>
2019-07-09 16:40:00 -0700

    Incorporate Chapter2 test fixes
    Closes #34

--- src/Chapter02.Tests/Listing02.02.Tests.cs
+++ src/Chapter02.Tests/Listing02.02.Tests.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_06.Tests
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_02.Tests
 {
     [TestClass]
     public class LiteralValueTests
     {
         [TestMethod]
-        public void Main_WriteHexadecimal()
+        public void Main_WriteNumbers()
         {
-            const string expected = "42";
+            const string expected = @"1.61803398874989";
 
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(
                 expected, Program.Main);
