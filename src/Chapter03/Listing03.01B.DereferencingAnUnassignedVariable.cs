fd1107b6ebcd4e1589aebd9eb1d9fe0d550a16bd
Mark Michaelis <mark@IntelliTect.com>
2019-07-20 05:49:53 -0700

    Commented out the code tha failes to comple - intentionally

--- src/Chapter03/Listing03.01B.DereferencingAnUnassignedVariable.cs
+++ src/Chapter03/Listing03.01B.DereferencingAnUnassignedVariable.cs
@@ -1,8 +1,5 @@
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter03.Listing03_01B
 {
-    // NOTE:    This file is referenced in Chapter03.csproj not to compile in order to prevent 
-    //          the build from failing.
-
     public class Program
     {
         public static void Main()
@@ -10,7 +7,7 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter03.Listing03_01B
             string? text;
             // ...
             // Compile Error: Use of unassigned local variable 'text'
-            System.Console.WriteLine(text.length);
+            // System.Console.WriteLine(text.length);
         }
     }
 }
