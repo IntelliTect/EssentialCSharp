14cd7760b661dc8ebff4b0dad1e52e507895eba8
Cameron J Osborn <cameron.osborn@intellitect.com>
2019-07-23 16:35:52 -0700

    Add tests chapter03 (#74)
    * Rename Listing03.01, 03.02 to add period
    
    * Add tests to Listings 02, 22, 26, 29

---
+++ src/Chapter03.Tests/Listing03.02.Tests.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter03.Listing03_02.Tests
+{
+    [TestClass]
+    public class UppercaseTests
+    {
+        [TestMethod]
+        public void Main_GivenValidString_MakeUppercase()
+        {
+            const string expected =
+@"Enter text: <<test
+>>TEST";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Uppercase.Main);
+        }
+    }
+}
