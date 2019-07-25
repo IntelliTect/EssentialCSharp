d4541f36283eef191514da52113e7f62cabb0a48
Cameron Osborn <cameron0401@gmail.com>
2019-07-25 16:53:22 -0700

    Make Listing31 public. Add tests 26,27,29,31

---
+++ src/Chapter06.Tests/Listing06.31.OverloadingAConstructor.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter06.Listing06_31.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void UnitTest1()
+        {
+            Assert.Fail();
+        }
+    }
+}
