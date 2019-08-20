a49826ac1317fb9c7d83c4897dad82c03d06f54a
Cameron J Osborn <cameron.osborn@intellitect.com>
2019-08-20 12:29:17 -0700

    Refactor v8 nullability net core fix (#86)
    * Use C#8 Bang operator to fix nullability
    
    * Nix support edge case

--- src/Shared/NetCore.cs
+++ src/Shared/NetCore.cs
@@ -6,10 +6,9 @@ public class NetCore
     public static string GetNetCoreVersion()
     {
         Assembly assembly = typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly;
-        var assemblyCodeBase = assembly.CodeBase ?? throw new ArgumentNullException("assemblyCodeBase");
-        string[] assemblyPath = assemblyCodeBase.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
+        string[] assemblyPath = assembly!.CodeBase!.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
         int netCoreAppIndex = Array.IndexOf(assemblyPath, "Microsoft.NETCore.App");
-        if (netCoreAppIndex > 0 && netCoreAppIndex < assemblyPath.Length - 2)
+        if (netCoreAppIndex >= 0 && netCoreAppIndex < assemblyPath.Length - 2)
             return assemblyPath[netCoreAppIndex + 1];
         throw new Exception("Unable to determine .NET Core version.");
     }
