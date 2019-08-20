021ffdaeec4187a81c620d0e9c7f3b86f06e162a
Cameron Osborn <cameron0401@gmail.com>
2019-08-20 11:52:35 -0700

    Change nameof

--- src/Shared/NetCore.cs
+++ src/Shared/NetCore.cs
@@ -6,7 +6,7 @@ public class NetCore
     public static string GetNetCoreVersion()
     {
         Assembly assembly = typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly;
-        var assemblyCodeBase = assembly.CodeBase ?? throw new ArgumentNullException(nameof(assembly.CodeBase));
+        var assemblyCodeBase = assembly.CodeBase ?? throw new ArgumentNullException("assemblyCodeBase");
         string[] assemblyPath = assemblyCodeBase.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
         int netCoreAppIndex = Array.IndexOf(assemblyPath, "Microsoft.NETCore.App");
         if (netCoreAppIndex > 0 && netCoreAppIndex < assemblyPath.Length - 2)
