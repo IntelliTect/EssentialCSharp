f9a6e18ab5b8dacba352a8d5f1cc30ea0bb035e0
Cameron Osborn <cameron0401@gmail.com>
2019-08-20 12:25:25 -0700

    Nix support edge case

--- src/Shared/NetCore.cs
+++ src/Shared/NetCore.cs
@@ -8,7 +8,7 @@ public class NetCore
         Assembly assembly = typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly;
         string[] assemblyPath = assembly!.CodeBase!.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
         int netCoreAppIndex = Array.IndexOf(assemblyPath, "Microsoft.NETCore.App");
-        if (netCoreAppIndex > 0 && netCoreAppIndex < assemblyPath.Length - 2)
+        if (netCoreAppIndex >= 0 && netCoreAppIndex < assemblyPath.Length - 2)
             return assemblyPath[netCoreAppIndex + 1];
         throw new Exception("Unable to determine .NET Core version.");
     }
