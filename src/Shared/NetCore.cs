50b6a6525eab30fd444ddae28f0767f3c5309772
Cameron Osborn <cameron0401@gmail.com>
2019-08-20 11:50:36 -0700

    Use conditional access to avoid nullability issues

--- src/Shared/NetCore.cs
+++ src/Shared/NetCore.cs
@@ -5,8 +5,9 @@ public class NetCore
 {
     public static string GetNetCoreVersion()
     {
-        var assembly = typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly;
-        var assemblyPath = assembly.CodeBase.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
+        Assembly assembly = typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly;
+        var assemblyCodeBase = assembly.CodeBase ?? throw new ArgumentNullException(nameof(assembly.CodeBase));
+        string[] assemblyPath = assemblyCodeBase.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
         int netCoreAppIndex = Array.IndexOf(assemblyPath, "Microsoft.NETCore.App");
         if (netCoreAppIndex > 0 && netCoreAppIndex < assemblyPath.Length - 2)
             return assemblyPath[netCoreAppIndex + 1];
