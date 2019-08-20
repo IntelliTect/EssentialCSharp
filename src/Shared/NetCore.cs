ddf6d76bdf9553419baf170dbcc896dcb98776b1
Cameron Osborn <cameron0401@gmail.com>
2019-08-20 12:20:47 -0700

    Use C#8 Bang operator to fix nullability

--- src/Shared/NetCore.cs
+++ src/Shared/NetCore.cs
@@ -6,8 +6,7 @@ public class NetCore
     public static string GetNetCoreVersion()
     {
         Assembly assembly = typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly;
-        var assemblyCodeBase = assembly.CodeBase ?? throw new ArgumentNullException("assemblyCodeBase");
-        string[] assemblyPath = assemblyCodeBase.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
+        string[] assemblyPath = assembly!.CodeBase!.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
         int netCoreAppIndex = Array.IndexOf(assemblyPath, "Microsoft.NETCore.App");
         if (netCoreAppIndex > 0 && netCoreAppIndex < assemblyPath.Length - 2)
             return assemblyPath[netCoreAppIndex + 1];
