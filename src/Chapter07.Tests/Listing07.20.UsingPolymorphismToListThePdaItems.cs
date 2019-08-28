55c2894109bdc1ed4cdaa8774af5aa25a91d6ccc
Cameron Osborn <cameron0401@gmail.com>
2019-08-28 14:16:48 -0700

    Attempt to handle culture difference

--- src/Chapter07.Tests/Listing07.20.UsingPolymorphismToListThePdaItems.cs
+++ src/Chapter07.Tests/Listing07.20.UsingPolymorphismToListThePdaItems.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Globalization;
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter07.Listing07_20.Tests
@@ -8,23 +11,24 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter07.Listing07_20.Tests
         [TestMethod]
         public void Main_WriteMultiplePdaItemToScreen()
         {
-            const string expected =
-@"________
+            DateTime startDateTime = new DateTime(2008, 7, 18);
+            DateTime endDateTime = startDateTime.AddDays(1);
+
+            string expected =
+$@"________
 FirstName: Sherlock
 LastName: Holmes
 Address: 221B Baker Street, London, England
 
 ________
 Subject: Soccer tournament
-Start: 07/18/2008 00:00:00
-End: 07/19/2008 00:00:00
+Start: {startDateTime}
+End: {endDateTime}
 Location: Estádio da Machava
 ________
 FirstName: Anne
 LastName: Frank
-Address: Apt 56B, Whitehaven Mansions, Sandhurst Sq, London
-
-";
+Address: Apt 56B, Whitehaven Mansions, Sandhurst Sq, London" + Environment.NewLine + Environment.NewLine;
 
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(
                 expected, Program.Main);
