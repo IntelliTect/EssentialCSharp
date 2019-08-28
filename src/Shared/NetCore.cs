99d95e087d372bc2107d8f58ac6d5d4229cee4f2
Mark Michaelis <mark@IntelliTect.com>
2019-08-28 16:45:35 -0700

    Added support for nullable to NetCore.cs.

--- src/Shared/NetCore.cs
+++ src/Shared/NetCore.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Reflection;
-
 public class NetCore
 {
     public static string GetNetCoreVersion()
     {
-        var assembly = typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly;
-        var assemblyPath = assembly.CodeBase.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
+        Assembly assembly = typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly;
+        string[]? assemblyPath = assembly.CodeBase?.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
         int netCoreAppIndex = Array.IndexOf(assemblyPath, "Microsoft.NETCore.App");
         if (netCoreAppIndex > 0 && netCoreAppIndex < assemblyPath.Length - 2)
             return assemblyPath[netCoreAppIndex + 1];
