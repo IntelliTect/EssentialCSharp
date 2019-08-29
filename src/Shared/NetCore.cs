336df1410a6518a97e9f553363c12bded14060dc
Mark Michaelis <mark@IntelliTect.com>
2019-08-29 02:46:29 -0700

    Switched Nullable support to be off by default for all projects; Added IgnoreExpectedWarnings option which will suppress warnings that are expected; Refactored packagereferences and PropertyGroup items that are common for all test projects into ChapterTests.props for all test projects Refactored common PropertyGroup items into Chapter.props for all Chapter projects.

--- src/Shared/NetCore.cs
+++ src/Shared/NetCore.cs
@@ -1,14 +1,14 @@
 using System;
 using System.Reflection;
-
+#nullable enable
 public class NetCore
 {
     public static string GetNetCoreVersion()
     {
         Assembly assembly = typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly;
-        string[] assemblyPath = assembly!.CodeBase!.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
+        string[] assemblyPath = assembly.CodeBase!.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
         int netCoreAppIndex = Array.IndexOf(assemblyPath, "Microsoft.NETCore.App");
-        if (netCoreAppIndex >= 0 && netCoreAppIndex < assemblyPath.Length - 2)
+        if (netCoreAppIndex > 0 && netCoreAppIndex < assemblyPath.Length - 2)
             return assemblyPath[netCoreAppIndex + 1];
         throw new Exception("Unable to determine .NET Core version.");
     }
