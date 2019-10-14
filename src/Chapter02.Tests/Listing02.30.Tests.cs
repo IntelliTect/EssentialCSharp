b9ed2ea8788b92e1926e00ba1a8cd67d63f6b55f
Mark Michaelis <mark@IntelliTect.com>
2019-10-13 20:24:52 -0700

    Added example that shows scope of out declared variable.

--- src/Chapter02.Tests/Listing02.30.Tests.cs
+++ src/Chapter02.Tests/Listing02.30.Tests.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_30.Tests
 {
@@ -8,7 +9,7 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_30.Tests
         [TestMethod]
         public void Main_UsingTryParseWithInlineOut_Pi()
         {
-            string expected = $"Enter a number: <<{System.Math.PI}>>input was parsed successfully to {System.Math.PI}.";
+            string expected = $"Enter a number: <<{System.Math.PI}>>input was parsed successfully to {System.Math.PI}.>>{Environment.NewLine}'number' currently has the value: { System.Math.PI }";
 
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(
                 expected, Program.Main);
@@ -18,7 +19,8 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_30.Tests
         [TestMethod]
         public void Main_UsingTryParseWithInlineOut_Pie()
         {
-            string expected = $"Enter a number: <<pie>>The text entered was not a valid number.";
+            string expected = @$"Enter a number: <<pie>>The text entered was not a valid number.
+'number' currently has the value: { default(int) }";
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(
                 expected, Program.Main);
 
