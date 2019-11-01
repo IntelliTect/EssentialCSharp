d963bab9859873b18fabebeb18fe837604c484c0
Mark Michaelis <mark@IntelliTect.com>
2019-11-01 13:14:11 -0700

    Refactoring tests in Chapter 19.

--- src/Chapter19.Tests/Listing19.13.Tests.cs
+++ src/Chapter19.Tests/Listing19.13.Tests.cs
@@ -1,79 +1,16 @@
+using AddisonWesley.Michaelis.EssentialCSharp.Chapter19.Listing19_13to14.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter19.Listing19_13.Tests
 {
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using System;
-    using System.Runtime.ExceptionServices;
-    using System.Threading.Tasks;
-
-    public class ProgramWrapper
-    {
-        Action<string[]> MainMethod { get; }
-        public ProgramWrapper(Action<string[]> mainMethod)
-        {
-            MainMethod = mainMethod;
-        }
-
-        public void Main(string[] args)
-        {
-            MainMethod(args);
-        }
-    }
 
     [TestClass]
-    public class ProgramTests
+    public class ProgramTests : BaseProgramTests
     {
-        public ProgramWrapper ProgramWrapper { get;set;}
-
-        [TestMethod]
-        public void ValueTaskAsyncReturnTest()
+       [ClassInitialize]
+       static public void ClassInitialize(TestContext textContext)
         {
-            string findText = "IntelliTect";
-            string expected = @$"Searching for {findText}...
-http://www.IntelliTect.com";
-
-            string actual = IntelliTect.TestTools.Console.ConsoleAssert.Execute("",
-            () =>
-            {
-                Program.Main(new string[] { findText });
-            });
-
-            IntelliTect.TestTools.Console.StringExtensions.IsLike(
-                $"{expected}...*", actual);
-            IntelliTect.TestTools.Console.StringExtensions.IsLikeRegEx(
-                @$"expected\.+^[1-9]\d*$", actual);
-        }
-
-        [TestMethod]
-        public void Main_FindTextDoesNotExist_NotFound()
-        {
-            string findText = "RANDOM TEXT NOT ON SITE";
-            string expected = @$"Searching for {findText}...
-http://www.IntelliTect.com";
-
-            string actual = IntelliTect.TestTools.Console.ConsoleAssert.Execute("",
-            () =>
-            {
-                Program.Main(new string[] { findText });
-            });
-
-            IntelliTect.TestTools.Console.StringExtensions.IsLike(
-                $"{expected}...*0", actual);
-            IntelliTect.TestTools.Console.StringExtensions.IsLikeRegEx(
-                @$"expected\.+0$", actual);
-        }
-
-        [TestMethod][ExpectedException(typeof(ArgumentException))]
-        public void Main_PassEmptyArray_ThrowException()
-        {
-            try
-            {
-                Program.Main(Array.Empty<string>());
-            }
-            catch(AggregateException exception)
-            {
-                ExceptionDispatchInfo.Throw(exception.Flatten().InnerExceptions[0]);
-            }
+            ProgramWrapper = new ProgramWrapper(Program.Main);
         }
     }
 }
--- src/Chapter19.Tests/Listing19.14.Tests.cs
+++ src/Chapter19.Tests/Listing19.14.Tests.cs
@@ -1,62 +1,16 @@
+using AddisonWesley.Michaelis.EssentialCSharp.Chapter19.Listing19_13to14.Tests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter19.Listing19_14.Tests
 {
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
-    using System;
-    using System.Runtime.ExceptionServices;
 
     [TestClass]
-    public class ProgramTests
+    public class ProgramTests : BaseProgramTests
     {
-        [TestMethod]
-        public void ValueTaskAsyncReturnTest()
+        [ClassInitialize]
+        static public void ClassInitialize(TestContext textContext)
         {
-            string findText = "IntelliTect";
-            string expected = @$"Searching for {findText}...
-http://www.IntelliTect.com";
-
-            string actual = IntelliTect.TestTools.Console.ConsoleAssert.Execute("",
-            () =>
-            {
-                Program.Main(new string[] { findText });
-            });
-
-            IntelliTect.TestTools.Console.StringExtensions.IsLike(
-                $"{expected}...*", actual);
-            IntelliTect.TestTools.Console.StringExtensions.IsLikeRegEx(
-                @$"expected\.+^[1-9]\d*$", actual);
-        }
-
-        [TestMethod]
-        public void Main_FindTextDoesNotExist_NotFound()
-        {
-            string findText = "RANDOM TEXT NOT ON SITE";
-            string expected = @$"Searching for {findText}...
-http://www.IntelliTect.com";
-
-            string actual = IntelliTect.TestTools.Console.ConsoleAssert.Execute("",
-            () =>
-            {
-                Program.Main(new string[] { findText });
-            });
-
-            IntelliTect.TestTools.Console.StringExtensions.IsLike(
-                $"{expected}...*0", actual);
-            IntelliTect.TestTools.Console.StringExtensions.IsLikeRegEx(
-                @$"expected\.+0$", actual);
-        }
-
-        [TestMethod][ExpectedException(typeof(ArgumentException))]
-        public void Main_PassEmptyArray_ThrowException()
-        {
-            try
-            {
-                Program.Main(Array.Empty<string>());
-            }
-            catch(AggregateException exception)
-            {
-                ExceptionDispatchInfo.Throw(exception.Flatten().InnerExceptions[0]);
-            }
+            ProgramWrapper = new ProgramWrapper(Program.Main);
         }
     }
 }
