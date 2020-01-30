c070924b9aab6fdad4380e7c52db8873ffbb7731
Mark Michaelis <mark@IntelliTect.com>
2020-01-29 19:30:18 -0800

    Addressed nullabilty in Chapter 17.

--- src/Chapter17/Listing17.19.CSharpEquivalentOfCompilerGeneratedCodeForIterators.cs
+++ src/Chapter17/Listing17.19.CSharpEquivalentOfCompilerGeneratedCodeForIterators.cs
@@ -1,104 +1,57 @@
+using AddisonWesley.Michaelis.EssentialCSharp.Chapter17.Listing17_10;
+using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter17.Listing17_19
 {
-    using Listing17_10;
-    using System;
-    using System.Collections.Generic;
-// Not concerned about Type naming for compiler simulated code
-#pragma warning disable CS0693
-
-    public class Pair<T> : IPair<T>, IEnumerable<T>
+    [NullableContext(1)]
+    [Nullable(0)]
+    public struct Pair<[Nullable(2)] T> : IPair<T>, IEnumerable<T>, IEnumerable
     {
-        // ...
-
-        // The iterator is expanded into the following
-        // code by the compiler
-
-        public virtual IEnumerator<T> GetEnumerator()
+        public Pair(T first, T second)
         {
-            __ListEnumerator<T> result = new __ListEnumerator<T>(0);
-            result._Pair = this;
-            return result;
+            First = first;
+            Second = second;
+        }
+
+        public T First { get; }
+
+        public T Second { get; }
+
+        public T this[PairItem index]
+        {
+            get
+            {
+                PairItem pairItem = index;
+                PairItem pairItem2 = pairItem;
+                T result;
+                if (pairItem2 != PairItem.First)
+                {
+                    if (pairItem2 != PairItem.Second)
+                    {
+                        throw new NotImplementedException(string.Format("The enum {0} has not been implemented", index.ToString()));
+                    }
+                    result = Second;
+                }
+                else
+                {
+                    result = First;
+                }
+                return result;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            yield return First;
+            yield return Second;
+            yield break;
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
         }
-
-        public T First { get; private set; }
-        public T Second { get; private set; }
-
-        #region Indexer
-        public T this[PairItem index]
-        {
-            get
-            {
-                switch(index)
-                {
-                    case PairItem.First:
-                        return First;
-                    case PairItem.Second:
-                        return Second;
-                    default:
-                        throw new NotImplementedException(
-                            string.Format(
-                            "The enum {0} has not been implemented",
-                            index.ToString()));
-                }
-            }
-        }
-        #endregion Indexer
-
-        private sealed class __ListEnumerator<T> : IEnumerator<T>
-        {
-            public __ListEnumerator(int itemCount)
-            {
-                _ItemCount = itemCount;
-            }
-
-            public Pair<T> _Pair;
-            T _Current;
-            int _ItemCount;
-
-            public void Reset()
-            {
-                throw new NotImplementedException();
-            }
-
-            public T Current
-            {
-                get { return _Current; }
-            }
-
-            object IEnumerator.Current
-            {
-                get { return Current; }
-            }
-
-            public bool MoveNext()
-            {
-                switch(_ItemCount)
-                {
-                    case 0:
-                        _Current = _Pair.First;
-                        _ItemCount++;
-                        return true;
-                    case 1:
-                        _Current = _Pair.Second;
-                        _ItemCount++;
-                        return true;
-                    default:
-                        return false;
-                }
-            }
-
-            public void Dispose()
-            {
-                throw new NotImplementedException();
-            }
-        }
     }
-#pragma warning restore CS0693
 }
