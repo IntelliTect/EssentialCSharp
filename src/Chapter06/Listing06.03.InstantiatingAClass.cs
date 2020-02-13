0582145d0fa9111251e0c3ff273969aa1ff43950
Mark Michaelis <mark@IntelliTect.com>
2020-02-13 11:35:21 -0800

    Minior IDE suggestion suppressed in Chapter 6.

--- src/Chapter06/Listing06.03.InstantiatingAClass.cs
+++ src/Chapter06/Listing06.03.InstantiatingAClass.cs
@@ -1,3 +1,6 @@
+// Justificaiton: only partial implementation provided for elucidation.
+#pragma warning disable IDE0060 // Remove unused parameter
+
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter06.Listing06_03
 {
     using Listing06_01;
@@ -11,6 +14,7 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter06.Listing06_03
             employee2 = new Employee();
 
             IncreaseSalary(employee1);
+            IncreaseSalary(employee2);
         }
 
         static void IncreaseSalary(Employee employee)
