21b101af14a1807fc7ee41e72b7938787d58c53f
Mark Michaelis <mark@IntelliTect.com>
2020-02-26 15:05:54 -0800

    Switched from CommandUtils sample to Logging sample.

--- src/Chapter10/Listing10.12.LeveragingADifferentLibrary.cs
+++ src/Chapter10/Listing10.12.LeveragingADifferentLibrary.cs
@@ -1,48 +1,23 @@
 ﻿namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter10.Listing10_12
 {
-    using System;
-    using System.Collections.Generic;
-    using Microsoft.Extensions.CommandLineUtils;
+    using Microsoft.Extensions.Logging;
 
     public sealed class Program
     {
         public static void Main(string[] args)
         {
-            CommandLineApplication commandLineApplication =
-                new CommandLineApplication(throwOnUnexpectedArg: false);
-            CommandArgument name = commandLineApplication.Argument(
-                "name", "Enter the full name of the person to be greeted.");
-            CommandOption greeting = commandLineApplication.Option(
-              "-$|-g |--greeting <greeting>",
-              "The greeting to display. The greeting supports"
-              + " a format string where '{name}' will be "
-              + "substituted with the name.",
-              CommandOptionType.SingleValue);
-            commandLineApplication.HelpOption("-? | -h | --help");
-            //commandLineApplication.OnExecute(() =>
-            //{
-            //    if (!greeting.HasValue())
-            //    {
-            //        Console.WriteLine($"Hello { name.Value }.");
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine(
-            //            greeting.Value().Replace("{name}", name.Value));
-            //    }
-            //    return 0;
-            //});
-            commandLineApplication.Execute(args);
+            using ILoggerFactory loggerFactory =
+                LoggerFactory.Create(builder => builder.AddConsole().AddDebug());
 
-            if (!greeting.HasValue())
-            {
-                Console.WriteLine($"Hello { name.Value }.");
-            }
-            else
-            {
-                Console.WriteLine(
-                    greeting.Value().Replace("{name}", name.Value));
-            }
+            ILogger logger = loggerFactory.CreateLogger(
+                categoryName: "Console");
+
+            logger.LogInformation($@"Hospital Emergency Codes: = '{
+                string.Join("', '", args)}'");
+            // ...
+
+            logger.LogWarning("This is a test of the emergency...");
+            // ...
         }
     }
 }
