2637459249b540d6692c5ed3cb2ca9cd48f484d2
Mark Michaelis <mark@IntelliTect.com>
2020-03-02 15:07:29 -0800

    Updated Chapter 12 with C# 8.0 related constraints.

--- src/Chapter12/Listing12.28.InheritedConstraintsSpecifiedExplicitly.cs
+++ src/Chapter12/Listing12.28.InheritedConstraintsSpecifiedExplicitly.cs
@@ -1,24 +1,18 @@
-namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter12.Listing12_28
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter12.Listing12_27
 {
     using System;
 
-    class EntityBase
+    class EntityBase<T> where T : IComparable<T>
     {
-        public virtual void Method<T>(T t)
-            where T : IComparable<T>
-        {
-            // ...
-        }
+        // ...
     }
 
-    class Order : EntityBase
-    {
-        public override void Method<T>(T t)
-        //    Constraints may not be repeated on overriding
-        //    members
-        //    where T : IComparable<T>
-        {
-            // ...
-        }
-    }
+    // ERROR: 
+    // The type 'T' must be convertible to 'System.IComparable<T>' 
+    // to use it as parameter 'T' in the generic type or 
+    // method
+    // class Entity<T> : EntityBase<T>
+    // {
+    //     ...
+    // }
 }
