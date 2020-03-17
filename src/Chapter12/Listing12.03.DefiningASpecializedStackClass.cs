8957dc40ab11b27dea5203e23765154d5799013d
twofingerrightclick <austensaffron@gmail.com>
2020-03-17 15:48:59 -0700

    Update Listing12.03.DefiningASpecializedStackClass.cs

--- src/Chapter12/Listing12.03.DefiningASpecializedStackClass.cs
+++ src/Chapter12/Listing12.03.DefiningASpecializedStackClass.cs
@@ -5,6 +5,6 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter12.Listing12_03
     public class CellStack
     {
         public virtual object Pop() { return new Cell(); } // would return that last cell added and remove it from the list
-        public virtual void Push(Cell obj) { }
+        public virtual void Push(Cell cell) { }
     }
 }
