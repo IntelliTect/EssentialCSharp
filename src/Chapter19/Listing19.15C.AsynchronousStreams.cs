b00ef879c3d418e03111d8e0842f7195ee87ab2f
Mark Michaelis <Mark@IntelliTect.com>
2020-03-31 03:28:53 -0700

    Added Listing 19.15C

---
+++ src/Chapter19/Listing19.15C.AsynchronousStreams.cs
@@ -0,0 +1,32 @@
+﻿namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter19.Listing19_15C
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Runtime.CompilerServices;
+    using AddisonWesley.Michaelis.EssentialCSharp.Shared;
+
+class AsyncEncryptionCollection : IAsyncEnumerable<string?>
+{
+    public async IAsyncEnumerator<string?> GetAsyncEnumerator(
+        CancellationToken cancellationToken = default)
+    {
+        yield return await Task<string>.Run(() => (string?)null);
+    }
+
+    static public async void Main()
+    {
+        AsyncEncryptionCollection collection =
+            new AsyncEncryptionCollection();
+        // ...
+
+        await foreach (string? fileName in collection)
+        {
+            Console.WriteLine(fileName);
+        }
+    }
+}
+}
