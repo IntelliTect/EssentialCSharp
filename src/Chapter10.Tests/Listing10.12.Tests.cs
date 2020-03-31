c2c9736f75e7d045ac24dcf238ee7bc62615c120
Cameron Osborn <cameron0401@gmail.com>
2020-03-31 12:09:42 -0700

    Remove VT100 chars before Assert

--- src/Chapter10.Tests/Listing10.12.Tests.cs
+++ src/Chapter10.Tests/Listing10.12.Tests.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using IntelliTect.TestTools.Console;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter10.Listing10_12.Tests
 {
@@ -13,11 +18,53 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter10.Listing10_12.Tests
                 @"info: Console[0]
       Hospital Emergency Codes: = 'black', 'blue', 'brown', 'CBR', 'orange', 'purple', 'red', 'yellow'
 warn: Console[0]
-      This is a test of the emergency...";
+      This is a test of the emergency...
+";
 
-            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
-                expected, (Action<string[]>)(Program.Main), 
-                new string[] { "black", "blue", "brown", "CBR", "orange", "purple", "red", "yellow" });
+            Action act = () => Program.Main(new[] {"black", "blue", "brown", "CBR", 
+                "orange", "purple", "red", "yellow"});
+
+            var result = Execute(act, true);
+            
+            Assert.AreEqual(expected, result);
+        }
+
+        private static string Execute(Action action, bool removeVT100 = false)
+        {
+            TextWriter savedOutputStream = Console.Out;
+            try
+            {
+                string output;
+                using (TextWriter writer = new StringWriter())
+                {
+                    Console.SetOut(writer);
+                    action();
+
+                    output = removeVT100 
+                        ? RemoveVT100(RemoveAll(writer.ToString(), (char) 27))
+                        : writer.ToString();
+                }
+
+                return output;
+            }
+            finally
+            {
+                Console.SetOut(savedOutputStream);
+            }
+        }
+
+        private static string RemoveAll(string removeFrom, char toRemove)
+        {
+            var characters = removeFrom.ToCharArray().ToList();
+
+            characters.RemoveAll(c => c == toRemove);
+
+            return new string(characters.ToArray());
+        }
+
+        private static string RemoveVT100(string removeFrom)
+        {
+            return Regex.Replace(removeFrom, @"\[\d{1,2}m", "");
         }
     }
 }
