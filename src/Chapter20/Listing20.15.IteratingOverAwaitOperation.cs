52585244ebeb3ed51afbb0abf92e244f3cbf6b00
twofingerrightclick <austensaffron@gmail.com>
2020-04-09 15:07:46 -0700

    Added 3 new Listings via their respective code in manuscript

---
+++ src/Chapter20/Listing20.15.IteratingOverAwaitOperation.cs
@@ -0,0 +1,50 @@
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter20.Listing20_15
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net.NetworkInformation;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        async private void PingButton_Click(
+        object sender, RoutedEventArgs e)
+        {
+            List<string> urls = new List<string>()
+      {
+          "www.habitat-spokane.org",
+          "www.partnersintl.org",
+          "www.iassist.org",
+          "www.fh.org",
+          "www.worldvision.org"
+      };
+            IPStatus status;
+
+            Func<string, Task<IPStatus>> func =
+                async (localUrl) =>
+                {
+                    Ping ping = new Ping();
+                    PingReply pingReply =
+                        await ping.SendPingAsync(localUrl);
+                    return pingReply.Status;
+                };
+
+            StatusLabel.Content = "Pinging…";
+
+            foreach (string url in urls)
+            {
+                status = await func(url);
+                StatusLabel.Text =
+                    $@"{ url }: { status.ToString() } ({
+                        Thread.CurrentThread.ManagedThreadId })";
+            }
+        }
+
+    }
+}
+
+
+
+
+
