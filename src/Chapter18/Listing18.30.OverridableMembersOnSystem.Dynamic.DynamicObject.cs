33de70d0aa3eb32d76fd2435d187a7e42779d5d5
twofingerrightclick <austensaffron@gmail.com>
2020-04-13 14:15:53 -0700

    Added listing 18.30 via copy from manuscript

---
+++ src/Chapter18/Listing18.30.OverridableMembersOnSystem.Dynamic.DynamicObject.cs
@@ -0,0 +1,43 @@
+﻿namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter18.Listing18_30
+{
+    using System.Collections.Generic;
+    using System.Dynamic;
+   
+    public class DynamicObject : IDynamicMetaObjectProvider
+    {
+        protected DynamicObject();
+
+        public virtual IEnumerable<string> GetDynamicMemberNames();
+        public virtual DynamicMetaObject GetMetaObject(
+            Expression parameter);
+        public virtual bool TryBinaryOperation(
+            BinaryOperationBinder binder, object arg,
+                out object result);
+        public virtual bool TryConvert(
+            ConvertBinder binder, out object result);
+        public virtual bool TryCreateInstance(
+            CreateInstanceBinder binder, object[] args,
+                  out object result);
+        public virtual bool TryDeleteIndex(
+            DeleteIndexBinder binder, object[] indexes);
+        public virtual bool TryDeleteMember(
+            DeleteMemberBinder binder);
+        public virtual bool TryGetIndex(
+            GetIndexBinder binder, object[] indexes,
+                  out object result);
+        public virtual bool TryGetMember(
+            GetMemberBinder binder, out object result);
+        public virtual bool TryInvoke(
+            InvokeBinder binder, object[] args, out object result);
+        public virtual bool TryInvokeMember(
+            InvokeMemberBinder binder, object[] args,
+                  out object result);
+        public virtual bool TrySetIndex(
+            SetIndexBinder binder, object[] indexes, object value);
+        public virtual bool TrySetMember(
+            SetMemberBinder binder, object value);
+        public virtual bool TryUnaryOperation(
+            UnaryOperationBinder binder, out object result);
+    }
+
+}
