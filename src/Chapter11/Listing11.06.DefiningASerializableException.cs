177203afb18364b0e03deb527b767e5f484f6638
Alexander Barbosa <40282381+lxbarbosa@users.noreply.github.com>
2020-05-02 15:58:48 -0300

    11.06 - Uncomment code

--- src/Chapter11/Listing11.06.DefiningASerializableException.cs
+++ src/Chapter11/Listing11.06.DefiningASerializableException.cs
@@ -1,20 +1,20 @@
-﻿//namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter11.Listing11_06
-//{
-//    using System;
-//    using System.Runtime.Serialization;
+//namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter11.Listing11_06
+{
+   using System;
+   using System.Runtime.Serialization;
 
-//    // Supporting serialization via an attribute
-//    [Serializable]
-//    class DatabaseException : System.ApplicationException
-//    {
-//        // ...
+   // Supporting serialization via an attribute
+   [Serializable]
+   class DatabaseException : System.ApplicationException
+   {
+       // ...
 
-//        // Used for deserialization of exceptions
-//        public DatabaseException(
-//            SerializationInfo serializationInfo,
-//            StreamingContext context)
-//        {
-//            //...
-//        }
-//    }
-//}
+       // Used for deserialization of exceptions
+       public DatabaseException(
+           SerializationInfo serializationInfo,
+           StreamingContext context)
+       {
+           //...
+       }
+   }
+}
