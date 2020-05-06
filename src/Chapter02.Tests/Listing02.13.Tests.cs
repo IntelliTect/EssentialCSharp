2773a13d9b692b53176c70dd2967d958834b0667
twofingerrightclick <austensaffron@gmail.com>
2020-05-06 15:18:32 -0700

    Added "Obsolete" to Listing02.13.Test

--- src/Chapter02.Tests/Listing02.13.Tests.cs
+++ src/Chapter02.Tests/Listing02.13.Tests.cs
@@ -7,12 +7,14 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_13.Tests
     public class DuelOfWitsTests
     {
         [TestMethod]
+        [Obsolete]
         public void Main_WriteDizzyQuote()
         {
             const string expected = "\"Truly, you have a dizzying intellect.\"\n\"Wait 'til I get going!\"\n";
 
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(
                 expected, (Action)DuelOfWits.Main, false);
+
         }
     }
 }
