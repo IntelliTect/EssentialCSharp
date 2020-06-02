e7e5fdbb5436e53b3e2bc2d0a23e1a15c981b749
Mark Michaelis <mark@IntelliTect.com>
2020-06-01 20:41:36 -0700

    Addressing IDE warnings #WIP

--- src/Chapter02/Listing02.29.UsingTryParseInPlaceOfAnInvalidCastException.cs
+++ src/Chapter02/Listing02.29.UsingTryParseInPlaceOfAnInvalidCastException.cs
@@ -1,3 +1,5 @@
+// Justification: Inline variable declaration not explained yet.
+#pragma warning disable IDE0018 // Inline variable declaration
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_29
 {
     public class Program
@@ -5,6 +7,7 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_29
         public static void Main()
         {
             double number;
+#pragma warning restore IDE0018 // Inline variable declaration
             string input;
 
             System.Console.Write("Enter a number: ");
