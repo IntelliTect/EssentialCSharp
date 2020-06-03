0ab92e6430129b8d8d18f1fbf7ba2c57ca65f722
twofingerrightclick <austensaffron@gmail.com>
2020-06-03 15:35:09 -0700

    Added missing 3.6 listing

---
+++ src/Chapter03/Listing03.05B.TheCSharpEquivalentOfCompilerGeneratedCILCodeForAValueTupleReturn.cs
@@ -0,0 +1,13 @@
+﻿namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter03.Listing03_05B
+{
+    namespace Chapter03
+    {
+        [return: System.Runtime.CompilerServices.TupleElementNames(new string[] { "First", "Second" })]
+        public System.ValueTuple<string, string> ParseNames(string fullName)
+        {
+            // ...
+        }
+
+    }
+
+}
