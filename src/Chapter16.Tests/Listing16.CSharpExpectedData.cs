c0982125e4d4477ed67a1db379d3697cb8489a41
Benjamin Michaelis <benjamin@michaelis.net>
2021-01-11 09:28:33 -0800

    Update Test to include "when"

--- src/Chapter16.Tests/Listing16.CSharpExpectedData.cs
+++ src/Chapter16.Tests/Listing16.CSharpExpectedData.cs
@@ -3,6 +3,6 @@
     public class CSharpExpectedData
     {
         public static readonly string ExpectedKeywords = "abstract as base bool break byte case catch char checked class const continue decimal default delegate do double else enum event explicit extern false finally fixed float for foreach goto if implicit in int interface internal is lock long namespace new null object operator out override params private protected public readonly ref return sbyte sealed short sizeof stackalloc static string struct switch this throw true try typeof uint ulong unchecked unsafe ushort using virtual void volatile while";
-        public static readonly string ExpectedContextualKeywords = "add alias ascending async await by descending dynamic equals from get global group into join let nameof nonnull on orderby partial remove select set value var where yield";
+        public static readonly string ExpectedContextualKeywords = "add alias ascending async await by descending dynamic equals from get global group into join let nameof nonnull on orderby partial remove select set value var when where yield";
     }
 }
