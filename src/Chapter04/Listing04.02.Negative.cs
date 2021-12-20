4255c58138ba2267584d5061efbaf32a325c7362
Benjamin Michaelis <benjamin@michaelis.net>
2021-12-19 17:48:29 -0800

    Updated value to match v8.0 book

--- src/Chapter04/Listing04.02.Negative.cs
+++ src/Chapter04/Listing04.02.Negative.cs
@@ -5,7 +5,7 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter04.Listing04_02
         public static void Main()
         {
             //National Debt to the Penny
-            decimal debt = -28382607800141.79M;
+            decimal debt = -26457079712930.80M;
 
             System.Console.WriteLine(debt);
         }
