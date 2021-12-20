c35e64a30ca3525051fe52f25a3e8544fe0a7a75
Benjamin Michaelis <benjamin@michaelis.net>
2021-12-19 17:52:54 -0800

    Update 3.27 to match 3.28

--- src/Chapter03/Listing03.27.Slicing.cs
+++ src/Chapter03/Listing03.27.Slicing.cs
@@ -6,7 +6,7 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter03.Listing03_27
         {
             string[] languages = new string[] {
                 "C#", "COBOL", "Java",
-                "C++", "TypeScript", "Visual Basic",
+                "C++", "TypeScript", "Swift",
                 "Python", "Lisp", "JavaScript"};
 
             System.Console.WriteLine($@"  0..3: {
@@ -16,7 +16,7 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter03.Listing03_27
                 string.Join(", ", languages[^3..^0]) // Python, Lisp, JavaScript
             }"); 
             System.Console.WriteLine($@" 3..^3: {
-                string.Join(", ", languages[3..^3]) // C++, TypeScript, Visual Basic
+                string.Join(", ", languages[3..^3]) // C++, TypeScript, Swift
             }");
             System.Console.WriteLine($@"  ..^6: {
                 string.Join(", ", languages[..^6])  // C#, COBOL, Java
@@ -25,11 +25,11 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter03.Listing03_27
                 string.Join(", ", languages[6..])  // Python, Lisp, JavaScript
             }");
             System.Console.WriteLine($@"    ..: {
-                // C#, COBOL, Java, C++, TypeScript, Visual Basic, Python, Lisp, JavaScript
+                // C#, COBOL, Java, C++, TypeScript, Swift, Python, Lisp, JavaScript
                 string.Join(", ", languages[..])  // Python, Lisp, JavaScript
             }");
             System.Console.WriteLine($@"    ..: {
-                // C#, COBOL, Java, C++, TypeScript, Visual Basic, Python, Lisp, JavaScript
+                // C#, COBOL, Java, C++, TypeScript, Swift, Python, Lisp, JavaScript
                 string.Join(", ", languages[0..^0])  // Python, Lisp, JavaScript
             }");
         }
