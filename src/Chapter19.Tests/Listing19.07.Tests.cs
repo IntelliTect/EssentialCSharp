ba88e2bc0556489caa5195647ee53a6a009e0157
Benjamin Michaelis <benjamin@michaelis.net>
2021-12-20 12:57:44 -0800

    19.09->19.07
    #TODO: Follow up with test

---
+++ src/Chapter19.Tests/Listing19.07.Tests.cs
@@ -0,0 +1,30 @@
+﻿
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter19.Listing19_07.Tests
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod][Ignore]
+        public void ValueTaskAsyncReturnTest()
+        {
+            string expected = @"3:0047:Throwing exception.
+3:0052:Unhandled exception handler starting.
+3:0055:Sleeping for 4000 ms
+1:0058:Sleeping for 2000 ms
+1:2059:Awake
+1:2060:Finally block running.
+3:4059:Awake
+Unhandled Exception: System.Exception: Exception of type 'System.Exception' was thrown.
+";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(expected,
+            () =>
+            {
+                Program.Main();
+            });
+
+        }
+    }
+}
