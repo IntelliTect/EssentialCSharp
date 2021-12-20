8efdcbdec1d15e1dab24de58efb3fb6ae1faeee2
Benjamin Michaelis <benjamin@michaelis.net>
2021-12-19 22:35:07 -0800

    19.05->19.03
    Changes to match book don't guarantee consistent finish order

--- src/Chapter19.Tests/Listing19.03.Test.cs
+++ src/Chapter19.Tests/Listing19.03.Test.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter19.Listing19_05.Tests
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter19.Listing19_03.Tests
 {
     [TestClass]
     public class ProgramTests
@@ -11,8 +11,8 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter19.Listing19_05.Tests
             string expected = @"Before
 Starting...
 Continuing A...
-Continuing B...
 Continuing C...
+Continuing B...
 Finished!";
 
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(expected,
