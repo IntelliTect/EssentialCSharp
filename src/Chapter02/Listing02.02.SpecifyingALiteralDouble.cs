6f90dd66146ee77afea04c77a517559bd9eed2ce
Benjamin Michaelis <benjamin@michaelis.net>
2021-12-19 19:03:11 -0800

    Update literal values to match concepts in new runtime behavior
    Turned off the ignore for the tests

--- src/Chapter02/Listing02.02.SpecifyingALiteralDouble.cs
+++ src/Chapter02/Listing02.02.SpecifyingALiteralDouble.cs
@@ -4,7 +4,7 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_02
     {
         public static void Main()
         {
-            System.Console.WriteLine(1.618033988749895);
+            System.Console.WriteLine(1.6180339887498948);
         }
     }
 }
