11741fc89279cc8b5a926102a49ac6eeb14b9b54
Benjamin Michaelis <benjamin@michaelis.net>
2021-12-19 17:40:44 -0800

    Fix Test 3.27

--- src/Chapter03.Tests/Listing03.27.Slicing.cs
+++ src/Chapter03.Tests/Listing03.27.Slicing.cs
@@ -11,11 +11,11 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter03.Listing03_27
             const string expected =
 @"  0..3: C#, COBOL, Java
 ^3..^0: Python, Lisp, JavaScript
- 3..^3: C++, TypeScript, Pascal
+ 3..^3: C++, TypeScript, Visual Basic
   ..^6: C#, COBOL, Java
    6..: Python, Lisp, JavaScript
-    ..: C#, COBOL, Java, C++, TypeScript, Pascal, Python, Lisp, JavaScript
-    ..: C#, COBOL, Java, C++, TypeScript, Pascal, Python, Lisp, JavaScript";
+    ..: C#, COBOL, Java, C++, TypeScript, Visual Basic, Python, Lisp, JavaScript
+    ..: C#, COBOL, Java, C++, TypeScript, Visual Basic, Python, Lisp, JavaScript";
 
             IntelliTect.TestTools.Console.ConsoleAssert.Expect(
                 expected, Program.Main);
