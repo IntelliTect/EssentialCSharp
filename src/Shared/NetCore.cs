e5ec5a338ef80530354fcf16b829420d4b307c6c
Benjamin Michaelis <benjamin@michaelis.net>
2021-12-27 22:16:58 -0800

    Activate Expected Warnings

--- src/Shared/NetCore.cs
+++ src/Shared/NetCore.cs
@@ -6,7 +6,7 @@ public static class NetCore
     public static string GetNetCoreVersion()
     {
         Assembly assembly = typeof(System.Runtime.GCSettings).GetTypeInfo().Assembly;
-        string[] assemblyPath = assembly.CodeBase!.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
+        string[] assemblyPath = assembly.Location!.Split(new[] { '/', '\\' }, StringSplitOptions.RemoveEmptyEntries);
         int netCoreAppIndex = Array.IndexOf(assemblyPath, "Microsoft.NETCore.App");
         if (netCoreAppIndex > 0 && netCoreAppIndex < assemblyPath.Length - 2)
             return assemblyPath[netCoreAppIndex + 1];
