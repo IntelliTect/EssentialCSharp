37682515096cc876616d8eb60fc06ae17b52ae1c
Mark Michaelis <mark@IntelliTect.com>
2021-12-30 15:02:30 -0800

    Updated to leverage Directory.Build.props file for TargetFrameworks.

--- src/Chapter10/Listing10.13.MakingTypesAvailableExternally.ps1
+++ src/Chapter10/Listing10.13.MakingTypesAvailableExternally.ps1
@@ -3,68 +3,6 @@ param(
     [int]$traceLevel
 )
 
-[string]$LibraryProjectName = 'GeoCoordinates'
-[string]$ConsoleProgramProjectName = 'GeoCoordinateProgram'
-[xml]$directoryBuildPropsXml = ([xml](Get-Content "$PSScriptRoot/../../Directory.Build.props"))
-[string]$langVersion = $directoryBuildPropsXml.Project.PropertyGroup.LangVersion
-[string[]]$frameworks = $directoryBuildPropsXml.Project.PropertyGroup.TargetFrameworks -split ';'
-[string]$SutCSFile = split-path -leaf $MyInvocation.MyCommand.Definition
-[string]$SutCSFile = "$PSScriptRoot/$([IO.Path]::GetFileNameWithoutExtension($SutCSFile)).cs"
-if(-not (Test-Path $SutCSFile)) { throw "Unable to find the file with the type to export ('$SutCSFile')"}
-
-function RunProject {
-    param(
-        $Framework
-    )
-
-    try {
-        Get-Item "$PSScriptRoot/$LibraryProjectName","$PSScriptRoot/$ConsoleProgramProjectName" -ErrorAction Ignore | Remove-Item  -Recurse
-        Set-PSDebug -Trace $traceLevel
-        
-        if([string]::IsNullOrEmpty($langVersion))
-        {
-            throw "LangVersion not set."
-        }
-        dotnet new Console --langVersion $langVersion --output "$ConsoleProgramProjectName" --framework $Framework
-        dotnet new ClassLib  --langVersion $langVersion --output "$LibraryProjectName" --framework $Framework
-        Remove-Item "$PSScriptRoot/$LibraryProjectName/class1.cs"
-
-        #New-Item -ItemType Directory "$PSScriptRoot/$LibraryProjectName"
-        $codeListing = @('namespace GeoCoordinates') + (
-            Get-Content $SutCSFile | 
-                Select-Object -Skip 1)
-        $codeListing > "$PSScriptRoot/$LibraryProjectName/GeoTypes.cs"
-        Get-Content "$PSScriptRoot/$LibraryProjectName/GeoTypes.cs"  # Display the listing
-        dotnet add "$PSScriptRoot/$ConsoleProgramProjectName/$ConsoleProgramProjectName.csproj" reference "$PSScriptRoot/$LibraryProjectName/$LibraryProjectName.csproj"
-        $codeListing = @"
-    namespace $ConsoleProgramProjectName
-    {
-        using $LibraryProjectName;
-        class HelloWorld
-        {
-            static void Main()
-            {
-                System.Type coordinateType = (new Coordinate()).GetType();
-                System.Console.WriteLine(
-                        $@"{coordinateType.Assembly} {System.Environment.NewLine
-                        }path='{coordinateType.Assembly.Location}'"
-                    );
-            }
-        }
-    }
-"@ 
-        $codeListing > "$PSScriptRoot/$ConsoleProgramProjectName/Program.cs"
-        Get-Content "$PSScriptRoot/$ConsoleProgramProjectName/Program.cs" # Display the listing
-        
-        dotnet build "$PSScriptRoot/$ConsoleProgramProjectName/$ConsoleProgramProjectName.csproj" -property:TargetFrameworks=$Framework
-        dotnet run --project "$PSScriptRoot/$ConsoleProgramProjectName/$ConsoleProgramProjectName.csproj" --no-build --framework $Framework
-
-    }
-    finally {
-        Set-PSDebug -Off
-    }
-}
-
 if('traceLevel' -notin $PSBoundParameters.Keys) {
     $traceLevel = Read-Host -Prompt @"
     Specifiy the trace level:
@@ -75,6 +13,69 @@ if('traceLevel' -notin $PSBoundParameters.Keys) {
 "@ 
 }
 
-$frameworks | Foreach-Object{
-    RunProject $_
+[string]$LibraryProjectName = 'GeoCoordinates'
+[string]$ConsoleProgramProjectName = 'GeoCoordinateProgram'
+
+[xml]$directoryBuildPropsXml = ([xml](Get-Content "$PSScriptRoot/../../Directory.Build.props"))
+[string]$langVersion = $directoryBuildPropsXml.Project.PropertyGroup.LangVersion
+# [string[]]$frameworks = $directoryBuildPropsXml.Project.PropertyGroup.TargetFrameworks -split ';'
+[string]$SutCSFile = split-path -leaf $MyInvocation.MyCommand.Definition
+[string]$SutCSFile = "$PSScriptRoot/$([IO.Path]::GetFileNameWithoutExtension($SutCSFile)).cs"
+if(-not (Test-Path $SutCSFile)) { throw "Unable to find the file with the type to export ('$SutCSFile')"}
+
+try {
+    Get-Item "$PSScriptRoot/$LibraryProjectName","$PSScriptRoot/$ConsoleProgramProjectName" -ErrorAction Ignore | Remove-Item  -Recurse
+    Set-PSDebug -Trace $traceLevel
+    
+    if([string]::IsNullOrEmpty($langVersion))
+    {
+        throw "LangVersion not set."
+    }
+
+    # Specyfing langVersion as dotnet new appears to ignore the Directory.Build.props file.
+    dotnet new Console --langVersion $langVersion --output "$ConsoleProgramProjectName"
+    $projectFilePath = "$PSScriptRoot/$ConsoleProgramProjectName/$($ConsoleProgramProjectName).csproj"
+    $updatedContent =(Get-Content $projectFilePath) | ? { $_ -notlike '*TargetFramework*'} 
+    Set-Content -Value $updatedContent -Path $projectFilePath
+
+    dotnet new ClassLib  --langVersion $langVersion --output "$LibraryProjectName"
+    $projectFilePath = "$PSScriptRoot/$LibraryProjectName/$($LibraryProjectName).csproj"
+    $updatedContent =(Get-Content $projectFilePath) | ? { $_ -notlike '*TargetFramework*'} 
+    Set-Content -Value $updatedContent -Path $projectFilePath
+    Remove-Item "$PSScriptRoot/$LibraryProjectName/class1.cs"
+
+    #New-Item -ItemType Directory "$PSScriptRoot/$LibraryProjectName"
+    $codeListing = @('namespace GeoCoordinates') + (
+        Get-Content $SutCSFile | 
+            Select-Object -Skip 1)
+    $codeListing > "$PSScriptRoot/$LibraryProjectName/GeoTypes.cs"
+    Get-Content "$PSScriptRoot/$LibraryProjectName/GeoTypes.cs"  # Display the listing
+    dotnet add "$PSScriptRoot/$ConsoleProgramProjectName/$ConsoleProgramProjectName.csproj" `
+        reference "$PSScriptRoot/$LibraryProjectName/$LibraryProjectName.csproj"
+    $codeListing = @"
+namespace $ConsoleProgramProjectName
+{
+    using $LibraryProjectName;
+    class HelloWorld
+    {
+        static void Main()
+        {
+            System.Type coordinateType = (new Coordinate()).GetType();
+            System.Console.WriteLine(
+                    $@"{coordinateType.Assembly} {System.Environment.NewLine
+                    }path='{coordinateType.Assembly.Location}'"
+                );
+        }
+    }
+}
+"@ 
+    $codeListing > "$PSScriptRoot/$ConsoleProgramProjectName/Program.cs"
+    Get-Content "$PSScriptRoot/$ConsoleProgramProjectName/Program.cs" # Display the listing
+    
+    dotnet build "$PSScriptRoot/$ConsoleProgramProjectName/$ConsoleProgramProjectName.csproj"
+    dotnet run --project "$PSScriptRoot/$ConsoleProgramProjectName/$ConsoleProgramProjectName.csproj" --no-build
+
+}
+finally {
+    Set-PSDebug -Off
 }
