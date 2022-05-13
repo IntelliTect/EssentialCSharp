01956d6ddbc18a181776ef4e7000ef745b51cfb1
Benjamin Michaelis <benjamin@michaelis.net>
2022-05-13 09:01:10 -0700

    Through 12.30

--- src/Chapter12/Listing12.27.RequiringADefaultConstructorConstraint.cs
+++ src/Chapter12/Listing12.27.RequiringADefaultConstructorConstraint.cs
@@ -2,29 +2,31 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter12.Listing12_26
 {
     using System;
     using System.Collections.Generic;
-
+    #region INCLUDE
     public class EntityBase<TKey>
         where TKey: notnull
     {
-        public TKey Key { get; set; }
-
         public EntityBase(TKey key)
         {
             Key = key;
         }
+        
+        public TKey Key { get; set; }
     }
 
     public class EntityDictionary<TKey, TValue> :
         Dictionary<TKey, TValue>
         where TKey : IComparable<TKey>, IFormattable
+        #region HIGHLIGHT
         where TValue : EntityBase<TKey>, new()
+        #endregion HIGHLIGHT
     {
-        // ...
-
         public TValue MakeValue(TKey key)
         {
+            #region HIGHLIGHT
             TValue newEntity = new TValue
             {
+            #endregion HIGHLIGHT
                 Key = key
             };
             Add(newEntity.Key, newEntity);
@@ -33,4 +35,5 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter12.Listing12_26
 
         // ...
     }
+    #endregion INCLUDE
 }
