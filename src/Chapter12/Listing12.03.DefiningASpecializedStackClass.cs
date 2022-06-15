6c3db5fab52a2b863747ca202d9bc40fd2ad472c
Hanna Michaelis <hanna@michaelis.net>
2022-06-15 10:50:01 -0700

    Changes from pdf review

--- src/Chapter12/Listing12.03.DefiningASpecializedStackClass.cs
+++ src/Chapter12/Listing12.03.DefiningASpecializedStackClass.cs
@@ -4,13 +4,8 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter12.Listing12_03
     #region INCLUDE
     public class CellStack
     {
-<<<<<<< Chapter13Regions
         public virtual Cell Pop() { return new Cell(); } // would return that last cell added and remove it from the list
         public virtual void Push(Cell cell) { }
-=======
-        public virtual object Pop() { return new Cell(); } // would return that last cell added and remove it from the list
-        public virtual void Push(Cell obj) { }
->>>>>>> Chapter 12 Regions through 12.6
         // ...
     }
     #endregion INCLUDE
