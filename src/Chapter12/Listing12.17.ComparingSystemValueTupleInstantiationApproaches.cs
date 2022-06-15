f0a395eb6bf881b9a192515bd9df1b76f255229c
Hanna Michaelis <hanna@michaelis.net>
2022-06-15 11:07:40 -0700

    More changes from pdf review

--- src/Chapter12/Listing12.17.ComparingSystemValueTupleInstantiationApproaches.cs
+++ src/Chapter12/Listing12.17.ComparingSystemValueTupleInstantiationApproaches.cs
@@ -8,12 +8,12 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter12.Listing12_17
     {
         public static void Main()
         {
-        #region INCLUDE
-        #if !PRECSHARP7
+            #region INCLUDE
+            #if !PRECSHARP7
             (string, Contact) keyValuePair;
             keyValuePair =
                 ("555-55-5555", new Contact("Inigo Montoya"));
-#else // Use System.ValueTupe<string,Contact> prior to C# 7.0
+            #else // Use System.ValueTupe<string,Contact> prior to C# 7.0
             ValueTuple<string, Contact> keyValuePair;
             keyValuePair =
             #region HIGHLIGHT
@@ -22,10 +22,10 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter12.Listing12_17
                     "555-55-5555", new Contact("Inigo Montoya"));
             keyValuePair =
             #region HIGHLIGHT
-             new ValueTuple<string, Contact>(
+                new ValueTuple<string, Contact>(
             #endregion HIGHLIGHT
                     "555-55-5555", new Contact("Inigo Montoya"));
-#endif // !PRECSHARP7
+            #endif // !PRECSHARP7
             #endregion INCLUDE
         }
     }
