c32045085dc0ef433d2364ce53e998a385c620aa
Benjamin Michaelis <benjamin@michaelis.net>
2022-07-01 19:34:22 -0700

    Update listing 18.11 (merge files and minor changes)

--- src/Chapter18/Listing18.11.SpecifyingAReturnAttribute.cs
+++ src/Chapter18/Listing18.11.SpecifyingAReturnAttribute.cs
@@ -1,13 +1,27 @@
+using System;
+
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter18.Listing18_11
 {
     public class Program
     {
         [return: Description(
            "Returns true if the object is in a valid state.")]
-        public bool IsValid()
+        public static bool IsValid()
         {
             // ...
             return true;
+        }
+    }
+    
+    public class DescriptionAttribute : Attribute
+    {
+        // TODO: Update listing in Manuscript
+        private readonly string _Description;
+        public string Description { get { return _Description; } }
+
+        public DescriptionAttribute(string description)
+        {
+            this._Description = description;
         }
     }
 }
