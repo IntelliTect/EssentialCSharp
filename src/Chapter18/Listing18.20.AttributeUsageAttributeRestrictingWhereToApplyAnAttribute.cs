df21119a4c37578d3d578cd33b14c8dafe6de387
HannaMichaelis <30376671+HannaMichaelis@users.noreply.github.com>
2022-07-11 11:03:30 -0700

    Update src/Chapter18/Listing18.20.AttributeUsageAttributeRestrictingWhereToApplyAnAttribute.cs
    Co-authored-by: Benjamin Michaelis <benjamin@michaelis.net>

--- src/Chapter18/Listing18.20.AttributeUsageAttributeRestrictingWhereToApplyAnAttribute.cs
+++ src/Chapter18/Listing18.20.AttributeUsageAttributeRestrictingWhereToApplyAnAttribute.cs
@@ -1,6 +1,6 @@
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter18.Listing18_20
 {
-    //will not compile if uncommented
+    // Will not compile if uncommented
     /*
     #region INCLUDE
     // ERROR: The attribute usage is restricted to properties
