2cbdbec70ca591f3d5ce4911a95a5c2ef96a7e3f
HannaMichaelis <30376671+HannaMichaelis@users.noreply.github.com>
2022-07-11 11:04:00 -0700

    Update src/Chapter18/Listing18.26.DynamicProgrammingUsingReflection.cs
    Co-authored-by: Benjamin Michaelis <benjamin@michaelis.net>

--- src/Chapter18/Listing18.26.DynamicProgrammingUsingReflection.cs
+++ src/Chapter18/Listing18.26.DynamicProgrammingUsingReflection.cs
@@ -16,8 +16,8 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter18.Listing18_26
             data = data * 3.5 + 28.6;
             //
             if(data == 2.4 + 112 + 26.2)
-                //The distance (in miles) for the swim, bike, and
-                //run portions of an Ironman traithlon, respectively
+                // The distance (in miles) for the swim, bike, and
+                // run portions of an Ironman traithlon, respectively
             {
                 Console.WriteLine(
                     $"{data} makes for a long triathlon.");
