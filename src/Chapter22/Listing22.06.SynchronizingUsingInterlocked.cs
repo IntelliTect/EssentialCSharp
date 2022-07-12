334380a2c8a547d5760be128a6b2ce0b637ab361
HannaMichaelis <30376671+HannaMichaelis@users.noreply.github.com>
2022-07-12 15:00:37 -0700

    Chapter22 regions (#296)
    * ch. 22 initial changes
    
    * ch. 22 code changes
    
    * 22.9 comments
    
    * Update src/Chapter22/Listing22.08.ThreadSafeEventNotification.cs
    
    Co-authored-by: Benjamin Michaelis <benjamin@michaelis.net>
    
    Co-authored-by: Benjamin Michaelis <benjamin@michaelis.net>

--- src/Chapter22/Listing22.06.SynchronizingUsingInterlocked.cs
+++ src/Chapter22/Listing22.06.SynchronizingUsingInterlocked.cs
@@ -4,13 +4,13 @@
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter22.Listing22_06
 {
     using System.Threading;
-
-    class SynchronizationUsingInterlocked
+    #region INCLUDE
+    public class SynchronizationUsingInterlocked
     {
         private static object? _Data;
 
         // Initialize data if not yet assigned
-        static void Initialize(object newValue)
+        public static void Initialize(object newValue)
         {
             // If _Data is null then set it to newValue
             Interlocked.CompareExchange(
@@ -19,4 +19,5 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter22.Listing22_06
 
         // ...
     }
+    #endregion INCLUDE
 }
--- src/Chapter22/Listing22.07.FiringEventNotification.cs
+++ src/Chapter22/Listing22.07.FiringEventNotification.cs
@@ -2,23 +2,27 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter22.Listing22_07
 {
     delegate void TemperatureChangedHandler(Program one, TemperatureEventArgs two);
 
-    class Program
+    public class Program
     {
         private static readonly TemperatureChangedHandler OnTemperatureChanged = delegate { };
 
         public void Main()
         {
+            #region INCLUDE
             // Not thread safe
-            if(OnTemperatureChanged != null) // Warning ignored to demonstrate problem.
+            #region HIGHLIGHT
+            if (OnTemperatureChanged != null)
+            #endregion HIGHLIGHT
             {
                 // Call subscribers
                 OnTemperatureChanged(
                     this, new TemperatureEventArgs(value));
             }
+            #endregion INCLUDE
         }
 
         // Justification: Lowercase to simulate the value keyword from a setter.
-        #pragma warning disable IDE1006 // Naming Styles
+#pragma warning disable IDE1006 // Naming Styles
         public object? value { get; set; }
         #pragma warning restore IDE1006 // Naming Styles
     }
