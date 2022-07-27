f86e32854d8e0a4f07e6fb0345c82614e0e2fcf7
Benjamin Michaelis <benjamin@michaelis.net>
2022-07-27 08:57:58 -0700

    Try enabling nullability with the props file to work on dotnet 5

--- src/Chapter10/Listing10.23.RegisteringAFinalizerWithProcessExit.ps1
+++ src/Chapter10/Listing10.23.RegisteringAFinalizerWithProcessExit.ps1
@@ -23,7 +23,7 @@ try {
     Get-Item "$PSScriptRoot/$ConsoleProgramProjectName" -ErrorAction Ignore | Remove-Item  -Recurse
     Set-PSDebug -Trace $traceLevel
     dotnet new Console --output "$PSScriptRoot/$ConsoleProgramProjectName"
-    Set-Content $PSScriptRoot/$ConsoleProgramProjectName/Directory.Build.props '<Project></Project>'
+    Set-Content ../../../../Chapter10/ProcessExitTestProgram/Directory.Build.props '<Project><PropertyGroup><Nullable>enable</Nullable></PropertyGroup></Project>'
 
     $SutCSFile = split-path -leaf $MyInvocation.MyCommand.Definition
     $SutCSFile = "$PSScriptRoot/$([IO.Path]::GetFileNameWithoutExtension($SutCSFile)).cs"
