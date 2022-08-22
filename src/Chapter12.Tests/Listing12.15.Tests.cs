8c2fd47e8da0233f52d9c540164e1b8c730cd880
Emma Spence <emma.spence@intellitect.com>
2022-08-22 13:29:07 -0700

    Add missing listing tests for chapters 12 & 13

---
+++ src/Chapter12.Tests/Listing12.15.Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter12.Listing12_15.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void MainTest()
+        {
+            const string expected = @"1914: Shackleton leaves for South Pole on ship Endurance";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Program.Main);
+        }
+    }
+}
