17ed93bb5845e1a52efaab50805486e4af4564e3
Emma Spence <emma.spence@intellitect.com>
2022-08-22 10:14:49 -0700

    Added in listing tests and privacy specification Chapter 6

---
+++ src/Chapter06.Tests/Listing06.01.Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter06.Listing06_01.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void MainTest()
+        {
+            const string expected = @"No output in this example";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Program.Main);
+        }
+    }
+}
---
+++ src/Chapter06.Tests/Listing06.34.Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter06.Listing06_34.Tests
+{
+    [TestClass]
+    public class EmployeeTests
+    {
+        [TestMethod]
+        public void MainTest()
+        {
+            const string expected = @"Inigo Montoya";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Program.Main);
+        }
+    }
+}
