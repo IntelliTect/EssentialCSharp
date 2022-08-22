a31a8f9034a3b864386b02a1ae60d163a8be21c6
espence2003 <88648165+espence2003@users.noreply.github.com>
2022-08-22 16:16:31 -0700

    Chapter12&13 missing tests (#332)
    * Add missing listing tests for chapters 12 & 13
    
    * Added missing tests for chapter 13
    
    Co-authored-by: Emma Spence <emma.spence@intellitect.com>

---
+++ src/Chapter13.Tests/Listing13.18.Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter13.Listing13_18.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void MainTest()
+        {
+            const string expected = @"";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Program.Main);
+        }
+    }
+}
