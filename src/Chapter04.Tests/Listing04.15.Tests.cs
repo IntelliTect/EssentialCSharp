d29a7a541848be9b7ae0a7e1eb643162eeef5ee4
espence2003 <88648165+espence2003@users.noreply.github.com>
2022-08-22 09:12:24 -0700

    Chapter 4: added missing tests. Both fail and pass properly. (#329)
    Co-authored-by: Emma Spence <emma.spence@intellitect.com>

---
+++ src/Chapter04.Tests/Listing04.15.Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter04.Listing04_15.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void MainTest()
+        {
+            const string expected = 
+                @"z=122	y=121	x=120	w=119	v=118	u=117	t=116	s=115	r=114	q=113	p=112	o=111	n=110	m=109	l=108	k=107	j=106	i=105	h=104	g=103	f=102	e=101	d=100	c=99	b=98	a=97	";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Program.Main);
+        }
+    }
+}
