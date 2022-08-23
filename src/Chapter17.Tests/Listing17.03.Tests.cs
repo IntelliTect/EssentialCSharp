fbca33c67428987e83201b30261d00cf1639b1f8
espence2003 <88648165+espence2003@users.noreply.github.com>
2022-08-23 14:48:01 -0700

    Add unit tests for listings in chapter 16 and 17 (#334)
    * Add unit tests for lisitngs in chapter 16 and 17
    
    * Got rid of incorrect tests for 16.06 and 16.09 listings
    
    Co-authored-by: Emma Spence <emma.spence@intellitect.com>

---
+++ src/Chapter17.Tests/Listing17.03.Tests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter17.Listing17_03.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void MainTest()
+        {
+            const string expected = @"private
+protected
+protected internal
+public";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Program.Main);
+        }
+    }
+}
