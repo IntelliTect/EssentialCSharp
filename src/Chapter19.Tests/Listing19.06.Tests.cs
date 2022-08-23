3eaa94cd5cffa7158dcee70895facdf6d528bd7f
Emma Spence <emma.spence@intellitect.com>
2022-08-23 14:49:22 -0700

    Added unit test for chapter 19

---
+++ src/Chapter19.Tests/Listing19.06.Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter19.Listing19_06.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void MainTest()
+        {
+            const string expected = @"ERROR: Operation is not valid due to the current state of the object.";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Program.Main);
+        }
+    }
+}
