79b904f111854b8aead66da5873a494401df6eed
Emma Spence <emma.spence@intellitect.com>
2022-08-23 16:03:34 -0700

    Added Missing unit tests for chapters 20 and 21

---
+++ src/Chapter20.Tests/Listing20.07.Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter20.Listing20_07.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void MainTest()
+        {
+            const string expected = @"";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, AsyncEncryptionCollection.Main);
+        }
+    }
+}
---
+++ src/Chapter21.Tests/Listing21.04.Tests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter21.Listing21_04.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void MainTest()
+        {
+            const string expected = @"ERROR: AggregateException:
+  UnauthorizedAccessException - Attempted to perform an unauthorized operation.
+  UnauthorizedAccessException - Attempted to perform an unauthorized operation.
+  UnauthorizedAccessException - Attempted to perform an unauthorized operation.
+  UnauthorizedAccessException - Attempted to perform an unauthorized operation.
+  UnauthorizedAccessException - Attempted to perform an unauthorized operation.
+  UnauthorizedAccessException - Attempted to perform an unauthorized operation.
+  UnauthorizedAccessException - Attempted to perform an unauthorized operation.
+  UnauthorizedAccessException - Attempted to perform an unauthorized operation.
+  UnauthorizedAccessException - Attempted to perform an unauthorized operation.";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Program.Main);
+        }
+    }
+}
