e1bf7a08fe8a04ccf007dd01c3d7080f81171559
espence2003 <88648165+espence2003@users.noreply.github.com>
2022-09-07 13:31:08 -0700

    Chapter 18 & 19 missing unit test (#335)
    Added missing unit tests for chapters 18 and 19. #12
    
    Co-authored-by: Emma Spence <emma.spence@intellitect.com>

---
+++ src/Chapter18.Tests/Listing18.26.Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter18.Listing18_26.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void MainTest()
+        {
+            string expected = $@"Hello!  My name is Inigo Montoya
+{140.6} makes for a long triathlon.";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Program.Main);
+        }
+    }
+}
---
+++ src/Chapter18.Tests/Listing18.28.Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter18.Listing18_28.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void MainTest()
+        {
+            const string expected = @"Inigo Montoya";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Program.Main);
+        }
+    }
+}
