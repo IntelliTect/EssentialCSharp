1edee24647a4411c3f12f09062ecb87a852822fb
Emma Spence <emma.spence@intellitect.com>
2022-09-08 10:41:45 -0700

    recommented 20.7 out

--- src/Chapter20.Tests/Listing20.07.Tests.cs
+++ src/Chapter20.Tests/Listing20.07.Tests.cs
@@ -1,16 +1,15 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Threading.Tasks;
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using System.Threading.Tasks;
 
-namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter20.Listing20_07.Tests
-{
-    [TestClass]
-    public class ProgramTests
-    {
-        [Ignore]
-        [TestMethod]
-        public async Task MainTest()
-        {
-            await IntelliTect.TestTools.Console.ConsoleAssert.ExpectAsync("<filename>", async () => await AsyncEncryptionCollection.Main());
-        }
-    }
-}
+//namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter20.Listing20_07.Tests
+//{
+//    [TestClass]
+//    public class ProgramTests
+//    {
+//        [TestMethod]
+//        public async Task MainTest()
+//        {
+//            await IntelliTect.TestTools.Console.ConsoleAssert.ExpectAsync("<filename>", async () => await AsyncEncryptionCollection.Main());
+//        }
+//    }
+//}
