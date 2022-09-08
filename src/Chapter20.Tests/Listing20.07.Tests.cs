576c0e4d2b011f05fc6318f8c646054fa670efdb
espence2003 <88648165+espence2003@users.noreply.github.com>
2022-09-08 13:30:33 -0700

    Added Missing unit tests for chapters 20 and 21 (#336)
    Created the missing unit tests for these chapters. #12
    
    Co-authored-by: Emma Spence <emma.spence@intellitect.com>

---
+++ src/Chapter20.Tests/Listing20.07.Tests.cs
@@ -0,0 +1,15 @@
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using System.Threading.Tasks;
+
+//namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter20.Listing20_07.Tests
+//{
+//    [TestClass]
+//    public class ProgramTests
+//    {
+//        [TestMethod]
+//        public async Task MainTest()
+//        {
+//            await IntelliTect.TestTools.Console.ConsoleAssert.ExpectAsync("<filename>", async () => await AsyncEncryptionCollection.Main());
+//        }
+//    }
+//}
---
+++ src/Chapter21.Tests/Listing21.01.Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter21.Listing21_01.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void MainTest()
+        {
+            const string expected = @"3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Program.Main);
+        }
+    }
+}
