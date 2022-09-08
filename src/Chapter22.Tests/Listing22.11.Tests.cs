9930d4aeba1907f1af827e3197f4ac5edd82a399
Emma Spence <emma.spence@intellitect.com>
2022-09-08 11:08:06 -0700

    Added chapters 22 and 23's missing unit tests

---
+++ src/Chapter22.Tests/Listing22.11.Tests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter22.Listing22_11.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void MainTest()
+        {
+            string expected = $@"Decrement Count = {-32767.01134}
+Main Count = {32767.01134}";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Program.Main);
+        }
+    }
+}
---
+++ src/Chapter23.Tests/Listing23.19.Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter23.Listing23_19.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void MainTest()
+        {
+            const string expected = "30° 18\' 0\"";
+
+            IntelliTect.TestTools.Console.ConsoleAssert.Expect(
+                expected, Program.Main);
+        }
+    }
+}
