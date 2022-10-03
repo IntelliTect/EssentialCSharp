9cb2d062f5e420970e969bd79baf09dc83b26834
Mark Michaelis <mark@IntelliTect.com>
2022-10-02 21:00:54 -0700

    Started initial draft with top level statement for initial listing

--- src/Chapter01.Tests/Listing01.02B.Tests.cs
+++ src/Chapter01.Tests/Listing01.02B.Tests.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter01.Listing01_06.Tests
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter01.Listing01_02B.Tests
 {
     [TestClass]
     public class HelloWorldTests
---
+++ src/Chapter01/Listing01.02C.MultipleStatementsOnSeparateLines.cs
@@ -0,0 +1,14 @@
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter01.Listing01_02C
+{
+    public class HelloWorld
+    {
+        public static void Main()
+        {
+            #region INCLUDE
+            System.Console.WriteLine("Down");
+            System.Console.WriteLine("Side");
+            System.Console.WriteLine("Up");
+            #endregion INCLUDE
+        }
+    }
+}
