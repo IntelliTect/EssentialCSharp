8cdbc6e942353002efb6597d335d2be44f04cd05
Mark Michaelis <mark@IntelliTect.com>
2022-10-10 17:20:38 -0700

    Updated to reflect VS new project generated file.

--- src/Chapter01/Listing01.01.HelloWorldInC#.cs
+++ src/Chapter01/Listing01.01.HelloWorldInC#.cs
@@ -1,3 +1,4 @@
+// See https://aka.ms/new-console-template for more information
 #region INCLUDE
-System.Console.WriteLine("Hello. My name is Inigo Montoya.");
+Console.WriteLine("Hello. My name is Inigo Montoya.");
 #endregion INCLUDE
