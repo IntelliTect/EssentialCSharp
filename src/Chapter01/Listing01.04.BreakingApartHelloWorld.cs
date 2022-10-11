d6925972f777fd6dee43e88aacf57fee0c614b9e
Mark Michaelis <mark@IntelliTect.com>
2022-10-10 17:21:14 -0700

    Added comments to identify various constructs

--- src/Chapter01/Listing01.04.BreakingApartHelloWorld.cs
+++ src/Chapter01/Listing01.04.BreakingApartHelloWorld.cs
@@ -1,12 +1,13 @@
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter01.Listing01_04
 {
     #region INCLUDE
-    public class Program//Class definition
+    public class Program                // BEGIN Class definition
     {
-        public static void Main()//Method declaration
-        {
-            System.Console.WriteLine("Hello, My name is Inigo Montoya");//This is the statement
-        }//This is the end of the method
-    }//end of the class definition
+        public static void Main()       // Method declaration
+        {                               // BEGIN method implementation
+            Console.WriteLine(          // This statement spans 2 lines
+                "Hello, My name is Inigo Montoya");
+        }                               // END method implementation
+    }                                   // END class definition
     #endregion INCLUDE
 }
