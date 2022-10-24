73d3dac151ece21ab871e6359e521fcd6ad714e0
Mark Michaelis <mark@IntelliTect.com>
2022-10-24 08:35:28 -0700

    Removed errant indent and whitespace

--- src/Chapter01/Listing01.09.RemovingWhitespace.cs
+++ src/Chapter01/Listing01.09.RemovingWhitespace.cs
@@ -1,7 +1,6 @@
-namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter01.Listing01_09
-{
-    #region INCLUDE
-    public class HelloWorld{public static void Main()
-{ Console.WriteLine("Hello Inigo Montoya");}}
-    #endregion INCLUDE
-}
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter01.Listing01_09;
+
+#region INCLUDE
+public class HelloWorld{public static void Main()
+{Console.WriteLine("Hello Inigo Montoya");}}
+#endregion INCLUDE
