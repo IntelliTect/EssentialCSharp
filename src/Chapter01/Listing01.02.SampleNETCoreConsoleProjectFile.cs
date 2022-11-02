d8cf587e9e9f4db60ad5e6e0331256c375be2781
Mark Michaelis <mark@IntelliTect.com>
2022-11-02 11:37:19 -0700

    Switched from cs to XML on csproj files
    These files are not used as project files, just sample files that are listed in the chapter.

--- src/Chapter01/Listing01.02.SampleNETCoreConsoleProjectFile.cs
+++
@@ -1,8 +0,0 @@
-#region INCLUDE
-<Project Sdk="Microsoft.NET.Sdk">
-    <PropertyGroup>
-        <OutputType>Exe</OutputType>
-        <TargetFramework>net7.0</TargetFramework>
-    </PropertyGroup>
-</Project>
-#endregion INCLUDE
