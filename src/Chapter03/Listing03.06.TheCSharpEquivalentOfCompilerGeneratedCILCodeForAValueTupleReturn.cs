6957637eacd1b21473df73e89f2c5c34e0b8543a
Mark Michaelis <mark@IntelliTect.com>
2022-11-03 22:46:53 -0700

    Mostly removed data type from array instantiation

--- src/Chapter03/Listing03.06.TheCSharpEquivalentOfCompilerGeneratedCILCodeForAValueTupleReturn.cs
+++ src/Chapter03/Listing03.06.TheCSharpEquivalentOfCompilerGeneratedCILCodeForAValueTupleReturn.cs
@@ -1,7 +1,7 @@
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter03.Listing03_06
 {
     #region INCLUDE
-    [return: System.Runtime.CompilerServices.TupleElementNames(new string[] { "First", "Second" })]
+    [return: System.Runtime.CompilerServices.TupleElementNames(new { "First", "Second" })]
     public System.ValueTuple<string, string> ParseNames(string fullName)
     {
         // ...
