a670af60c010636ea593add0683c7455ddc7e466
Mark Michaelis <mark@IntelliTect.com>
2022-11-05 21:03:06 -0800

    Adjusted class and method outside the Include region
    Additional include region adjustments

--- src/Chapter02/Listing02.22.OverflowingAnIntegerValue.cs
+++ src/Chapter02/Listing02.22.OverflowingAnIntegerValue.cs
@@ -1,15 +1,15 @@
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter02.Listing02_22
 {
-    #region INCLUDE
     public class Program
     {
         public static void Main()
         {
+            #region INCLUDE
             // int.MaxValue equals 2147483647
             int n = int.MaxValue;
             n = n + 1;
             Console.WriteLine(n);
+            #endregion INCLUDE
         }
     }
-    #endregion INCLUDE
 }
