d5491d55c470be3368dc3b736753f9a14994fa49
Mark Michaelis <mark@IntelliTect.com>
2022-11-06 19:54:07 -0800

    Removed old file references from Chapter 01 project file

--- src/Chapter01/Listing01.22.SampleCILOutput.cs
+++ src/Chapter01/Listing01.22.SampleCILOutput.cs
@@ -1,6 +1,5 @@
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter01.Listing01_22
 {
-/*
 #region INCLUDE
 .assembly extern System.Runtime
 {
@@ -71,5 +70,4 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter01.Listing01_22
     } // End of method System.Void HelloWorld.Program::.ctor()
 } // End of class HelloWorld.Program
 #endregion INCLUDE
-*/
 }
