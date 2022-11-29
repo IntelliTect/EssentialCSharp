0be89e1424a20ead727f89be3c1eb61b25b9a3c4
Mark Michaelis <mark@IntelliTect.com>
2022-11-28 17:16:12 -0800

    Use CompileAssert to verify error behavior.

---
+++ src/Chapter06.Tests/Listing06.15.Tests.cs
@@ -0,0 +1,19 @@
+﻿using AddisonWesley.Michaelis.EssentialCSharp.Shared;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Reflection.Emit;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter06.Listing06_15.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        async public Task UnassignedVariableThrowsError()
+        {
+            _ = await CompilerAssert.ExpectErrorsInFileAsync(
+                "Listing06.15.UsingThePrivateAccessModifier.cs",
+                new CompileError("CS0122", "'Employee.Password' is inaccessible due to its protection level"));
+
+        }
+    }
+}
