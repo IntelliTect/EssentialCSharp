b77149df624e24ca078e855857dd9450e6b328d5
Mark Michaelis <mark@IntelliTect.com>
2022-12-01 22:01:31 -0800

    Initial code for required

--- src/Chapter06/Listing06.34A.RequiredProperties.cs
+++ src/Chapter06/Listing06.34A.RequiredProperties.cs
@@ -1,42 +1,108 @@
 #if NET7_0_OR_GREATER
+using System.Diagnostics.CodeAnalysis;
+
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter06.Listing06_34A
 {
-    using System;
-    using System.Diagnostics.CodeAnalysis;
-
     public class Program
     {
         public static void Main()
         {
-            Employee employee = new("Inigo Montoya");
-
-            System.Console.WriteLine(employee.Name);
-
-            // ...
+            Book book1 = 
+                new() { Isbn= "Essential C#",Title= "978-0135972267" };
+            book1 =
+                 new() { Isbn= "Essential C#", Title= "978-0135972267" };
+            //book1 =
+            //    new(42);
         }
     }
+
+    /* 
+    GUIDELINES
+    DO NOT use constructor parameters to initialize required properties, instead rely on initializer specified values.
+    DO NOT use SetRequiredParameters unless all required parameters are assigned valid values during construction (the nullability warnings will help identify the required non-nullable reference type properties.
+    Not Proposed: "DO NOT use SetRequiredMembers unless there is a valid value you can assign to all required properties during construction."
+    Not Proposed: CONSIDER required properties with a default constructor to force the use of an initializer rather than a constructor for all required properties.
+    */
+
     #region INCLUDE
-    public class Employee
+    public class Book
     {
-        [SetsRequiredMembers]
-        public Employee(string name)
+        //public Book(string title, string isbn)
+        //{
+        //    Title = title;
+        //    ISBN = isbn;
+        //}
+
+        //#region HIGHLIGHT
+        //public Book(
+        //    int id, string firstName, string lastName)
+        //    : this(firstName, lastName)
+        //#endregion HIGHLIGHT
+        //{
+        //    Id = id;
+        //}
+
+        public Book()
         {
-            #region HIGHLIGHT
-            Name = name;
-            #endregion HIGHLIGHT
+            //Isbn = isbn;
+            //Title = title;
+
+            // Look up employee name...
+            // ...
         }
 
-        required public string Name
+        [SetsRequiredMembers]
+        public Book(int id)
         {
-            #region HIGHLIGHT
-            get => _Name!;
-            #endregion HIGHLIGHT
-            set => _Name =
-                #region HIGHLIGHT
-                value ?? throw new ArgumentNullException(nameof(value));
-            #endregion HIGHLIGHT
+            Isbn = "ISBN";
+            Title = "title";
+
+            // Look up employee name...
+            // ...
         }
-        private string? _Name;
+
+        //#pragma warning disable CS8618
+        [SetsRequiredMembers]
+        public Book(string isbn)
+        {
+            Isbn = isbn;
+
+            // Look up employee name...
+            // ...
+            Title = "Title";
+        }
+        //#pragma warning restore CS8618
+
+        // public int Id { get; init; }
+        string? _Title;
+        public required string Title 
+        { get
+            { 
+                return _Title!; 
+            } 
+            set
+            {
+                ArgumentException.ThrowIfNullOrEmpty(
+                    value = value?.Trim()!);
+                _Title = value;
+            }
+        }
+        string? _Isbn;
+        public required string Isbn
+        {
+            get
+            {
+                return _Isbn!;
+            }
+            set
+            {
+                ArgumentException.ThrowIfNullOrEmpty(
+                    value = value?.Trim()!);
+                _Isbn = value;
+            }
+        }
+        public string? Subtitle { get; set; } = "Not Enough";
+
         // ...
     }
     #endregion INCLUDE
