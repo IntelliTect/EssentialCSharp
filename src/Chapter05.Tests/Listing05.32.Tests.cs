6190de2a7a720ddb768f937d51e5befd1b8d0f00
Zack <zack.ward@intellitect.com>
2022-12-02 14:30:19 -0800

    5.31-33 Tests

--- src/Chapter05.Tests/Listing05.32.Tests.cs
+++ src/Chapter05.Tests/Listing05.32.Tests.cs
@@ -5,5 +5,22 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter05.Listing05_32.Tests;
 [TestClass]
 public class ProgramTests
 {
+    [TestMethod]
+    public void Main_NoArgs_ExpectErrors()
+    {
+        string expected = "Value cannot be null. (Parameter \'httpsUrl\')";
 
+        IntelliTect.TestTools.Console.ConsoleAssert.Expect(expected,
+        () =>
+        {
+            try
+            {
+                Program.Main();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        });
+    }
 }
