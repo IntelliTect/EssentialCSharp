5b71c8a252c481abce94a0891757c6481f5607e8
Mark Michaelis <mark@IntelliTect.com>
2022-12-05 03:29:36 -0800

    Static method extensions.

--- src/Chapter06/Listing06.42.DefiningAStaticMethodOnDirectoryInfo.cs
+++ src/Chapter06/Listing06.42.DefiningAStaticMethodOnDirectoryInfo.cs
@@ -3,71 +3,72 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter06.Listing06_42
     using System;
     using System.IO;
 
+
+    #region INCLUDE
+    public static class DirectoryInfoExtension
+    {
+        #region HIGHLIGHT
+        public static void CopyTo(
+            DirectoryInfo sourceDirectory, string target,
+            SearchOption option, string searchPattern)
+        #endregion HIGHLIGHT
+        {
+            if (target[^1] !=
+                Path.DirectorySeparatorChar)
+            {
+                target += Path.DirectorySeparatorChar;
+            }
+            if (!Directory.Exists(target))
+            {
+                Directory.CreateDirectory(target);
+            }
+
+            for (int i = 0; i < searchPattern.Length; i++)
+            {
+                foreach (string file in
+                    Directory.GetFiles(
+                        sourceDirectory.FullName, searchPattern))
+                {
+                    File.Copy(file,
+                        target + Path.GetFileName(file), true);
+                }
+            }
+
+            // Copy subdirectories (recursively)
+            if (option == SearchOption.AllDirectories)
+            {
+                foreach (string element in
+                    Directory.GetDirectories(
+                        sourceDirectory.FullName))
+                {
+                    Copy(element,
+                        target + Path.GetFileName(element),
+                        searchPattern);
+                }
+            }
+        }
+        #region EXCLUDE
+        private static void Copy(string element, string fileName, string searchPattern)
+        {
+            Console.WriteLine(
+                $"Copying(element: {element}, fileName: {fileName}, searchPattern: {searchPattern})");
+        }
+        #endregion EXCLUDE
+    }
+
     public class Program
     {
-        #region INCLUDE
-        public static class DirectoryInfoExtension
+        public static void Main(params string[] args)
         {
-            #region HIGHLIGHT
-            public static void CopyTo(
-                DirectoryInfo sourceDirectory, string target,
-                SearchOption option, string searchPattern)
-            #endregion HIGHLIGHT
-            {
-                if (target[target.Length - 1] !=
-                    Path.DirectorySeparatorChar)
-                {
-                    target += Path.DirectorySeparatorChar;
-                }
-                if(!Directory.Exists(target))
-                {
-                    Directory.CreateDirectory(target);
-                }
+            DirectoryInfo source = new(args[0]);
+            string target = args[1];
 
-                for(int i = 0; i < searchPattern.Length; i++)
-                {
-                    foreach(string file in
-                        Directory.GetFiles(
-                            sourceDirectory.FullName, searchPattern))
-                    {
-                        File.Copy(file,
-                            target + Path.GetFileName(file), true);
-                    }
-                }
-
-                // Copy subdirectories (recursively)
-                if(option == SearchOption.AllDirectories)
-                {
-                    foreach(string element in
-                        Directory.GetDirectories(
-                            sourceDirectory.FullName))
-                    {
-                        Copy(element,
-                            target + Path.GetFileName(element),
-                            searchPattern);
-                    }
-                }
-            }
-            #region EXCLUDE
-            private static void Copy(string element, string fileName, string searchPattern)
-            {
-                Console.WriteLine("Copying " + fileName);
-            }
-            #endregion EXCLUDE
-        }
-
-        #region EXCLUDE
-        public static void Main()
-        {
-        #endregion EXCLUDE
-            DirectoryInfo directory = new(".\\Source");
-            directory.MoveTo(".\\Root");
             #region HIGHLIGHT
             DirectoryInfoExtension.CopyTo(
-                directory, ".\\Target",
+                source, target,
                 SearchOption.AllDirectories, "*");
             #endregion HIGHLIGHT
         }
-        #endregion INCLUDE
     }
+    #endregion INCLUDE
 }
