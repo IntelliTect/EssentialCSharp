f459651636e50953805208516f25423e3db332f1
Mark Michaelis <mark@IntelliTect.com>
2022-12-05 03:29:36 -0800

    Static method extensions.

---
+++ src/Chapter06.Tests/Listing06.42.Tests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter06.Listing06_42.Tests;
+
+[TestClass]
+public class DirectoryTests
+{
+    [TestMethod]
+    public void MainTest()
+    {
+        string source = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+        string target = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+        try
+        {
+            Directory.CreateDirectory(source);
+            Directory.CreateDirectory(target);
+            IntelliTect.TestTools.Console.ConsoleAssert.Execute(null, () => Program.Main(source, target));
+        }
+        finally
+        {
+            if(Directory.Exists(source)) { Directory.Delete(source, recursive: true); }
+            if(Directory.Exists(target)) { Directory.Delete(target, recursive: true); }
+        }
+    }
+}
