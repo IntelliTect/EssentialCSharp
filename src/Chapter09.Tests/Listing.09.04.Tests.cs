7e7457c29813bc0e4f12177de851bf8d258ad184
Mark Michaelis <mark@IntelliTect.com>
2023-01-23 09:32:08 -0800

    Record Struct Code Listings Added

---
+++ src/Chapter09.Tests/Listing.09.04.Tests.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_04.Tests;
+
+[TestClass]
+public class BookTests
+{
+    [TestMethod]
+    public void Title_Null_ThrowsArgumentNullException() => _ = new Angle(0,0,0);
+    
+    [TestMethod]
+    public void ToString_PurpleCrayon_PrefixByFullName()
+    {
+        string expected = "AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_04.Angle { Name = , Degrees = 90, Minutes = 0, Seconds = 0 }";
+        string actual = new Angle(90, 0, 0).ToString();
+        Assert.AreEqual<string>(
+            expected,
+            actual);
+    }
+
+    [TestMethod]
+    public void Equals_CopyWithSameDegreesMinutesSecondsBut_AreEqual()
+    {
+        Angle book1 = new(0,0,0, "90 degrees");
+        Angle book2 = new(0, 0, 0);
+        Assert.AreEqual<Angle>(book1, book2);
+    }
+}
---
+++ src/Chapter09/Listing.09.04.CustomizingARecord.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_04;
+
+public readonly record struct Angle(int Degrees, int Minutes, int Seconds, string? Name = null)
+{
+    // Default constructor is not invoked unless
+    // called explicitly by the instantiation.
+    public Angle() : this(-42, 0, 0, null) { }
+
+    public Angle(string degrees, string? minutes, string seconds)
+        : this(degrees.Length, minutes!.Length, seconds.Length)
+    {
+    }
+    
+    public int Degrees { get; } = Degrees;
+
+    private readonly int _Minutes = Minutes;
+    public int Minutes
+    {
+        get => _Minutes;
+        init => _Minutes=value;
+    }
+    
+    private readonly int _Seconds = Seconds;
+    public int Seconds
+    {
+        get => _Seconds;
+        init => _Seconds=value;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder stringBuilder = new(GetType().FullName);
+        stringBuilder.Append(" { ");
+        if (PrintMembers(stringBuilder))
+        {
+            stringBuilder.Append(' ');
+        }
+        stringBuilder.Append('}');
+        return stringBuilder.ToString();
+    }
+    public bool Equals(Angle other)
+    {
+        return EqualityComparer<int>.Default.Equals(Degrees, other.Degrees)
+            && EqualityComparer<int>.Default.Equals(_Minutes, other._Minutes)
+            && EqualityComparer<int>.Default.Equals(_Seconds, other._Seconds);
+    }
+
+    public override int GetHashCode() => Seconds.GetHashCode();
+}
+
