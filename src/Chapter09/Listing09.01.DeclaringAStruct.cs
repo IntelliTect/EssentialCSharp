f115c849ade7affcf0fca20a73bae9ba42180595
Mark Michaelis <mark@IntelliTect.com>
2023-01-23 09:31:23 -0800

    Temporarily relocate original files

--- src/Chapter09/Listing09.01.DeclaringAStruct.cs
+++ src/Chapter09/Listing09.01.DeclaringAStruct.cs
@@ -1,39 +1,38 @@
-﻿namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_01
-{
-    #region INCLUDE
-    // Use keyword struct to declare a value type
-    #region HIGHLIGHT
-    readonly public struct Angle
-    #endregion HIGHLIGHT
-    {
-        public Angle(int degrees, int minutes, int seconds)
-        {
-            Degrees = degrees;
-            Minutes = minutes;
-            Seconds = seconds;
-        }
+﻿//namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_01;
 
-        public int Degrees { get; }
-        public int Minutes { get; }
-        public int Seconds { get; }
+//#region INCLUDE
+//// Use keyword struct to declare a value type
+//#region HIGHLIGHT
+//readonly public struct Angle
+//#endregion HIGHLIGHT
+//{
+//    public Angle(int degrees, int minutes, int seconds)
+//    {
+//        Degrees = degrees;
+//        Minutes = minutes;
+//        Seconds = seconds;
+//    }
 
-        public Angle Move(int degrees, int minutes, int seconds)
-        {
-            return new Angle(
-                Degrees + degrees,
-                Minutes + minutes,
-                Seconds + seconds);
-        }
-    }
+//    public int Degrees { get; }
+//    public int Minutes { get; }
+//    public int Seconds { get; }
 
-    // Declaring a class as a reference type
-    // (declaring it as a struct would create a value type
-    // larger than 16 bytes.)
-    class Coordinate
-    {
-        public Angle Longitude { get; set; }
+//    public Angle Move(int degrees, int minutes, int seconds)
+//    {
+//        return new Angle(
+//            Degrees + degrees,
+//            Minutes + minutes,
+//            Seconds + seconds);
+//    }
+//}
 
-        public Angle Latitude { get; set; }
-    }
-    #endregion INCLUDE
-}
+//// Declaring a class as a reference type
+//// (declaring it as a struct would create a value type
+//// larger than 16 bytes.)
+//class Coordinate
+//{
+//    public Angle Longitude { get; set; }
+
+//    public Angle Latitude { get; set; }
+//}
+//#endregion INCLUDE
---
+++ src/Chapter09/Original09/Listing09.07.AvoidingUnboxingAndCopying.cs
@@ -0,0 +1,23 @@
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_07;
+
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        #region INCLUDE
+        int number;
+        object thing;
+        number = 42;
+        // Boxing
+        thing = number;
+        #region HIGHLIGHT
+        // No unboxing conversion
+        #endregion HIGHLIGHT
+        string text = ((IFormattable)thing).ToString(
+            "X", null);
+        Console.WriteLine(text);
+        #endregion INCLUDE
+    }
+}
