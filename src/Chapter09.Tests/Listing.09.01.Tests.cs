8a855fbf7718753153a0cdad6d017b4e56375d9d
Mark Michaelis <mark@IntelliTect.com>
2023-01-23 09:32:08 -0800

    Record Struct Code Listings Added

---
+++ src/Chapter09.Tests/Listing.09.01.Tests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_01.Tests;
+
+[TestClass]
+public class AngleTests
+{
+    [TestMethod]
+    public void Create_AngleArray_ElementsAreUninitialized()
+    {
+        Angle angle = default;
+        Angle[] angles = new Angle[1];
+        Assert.AreEqual(angle, angles[0]);
+    }
+
+    [TestMethod]
+    public void Equals_CopyWithSameDegreesMinutesSeconds_AreEqual()
+    {
+        Angle angle1 = new(0, 0, 0);
+        Angle angle2 = new(0, 0, 0);
+        Assert.AreEqual<Angle>(angle1, angle2);
+    }
+}
---
+++ src/Chapter09.Tests/Listing.09.04.Tests.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_04.Tests;
+
+[TestClass]
+public class BookTests
+{
+    [TestMethod]
+    public void Title_Null_ThrowsArgumentNullException() => _ = new Angle(0,0,0);
+    
+    [TestMethod]
+    public void ToString_90Degrees_90DegreesFormatted()
+    {
+        string expected = $"90° 0' 0\"";
+        string actual = new Angle(90, 0, 0).ToString();
+        Assert.AreEqual<string>(
+            expected,
+            actual);
+    }
+    [TestMethod]
+    public void ToString_90DegreesWithName_90DegreesFormatted()
+    {
+        string expected = $"RightAngle: 90° 0' 0\"";
+        string actual = new Angle(90, 0, 0, "RightAngle").ToString();
+        Assert.AreEqual<string>(
+            expected,
+            actual);
+    }
+
+    [TestMethod]
+    public void Equals_CopyWithSameDegreesMinutesSecondsBut_AreEqual()
+    {
+        Angle book1 = new(0,0,0, "90 degrees");
+        Angle book2 = new(0, 0, 0);
+        Assert.AreEqual<Angle>(book1, book2);
+    }
+}
