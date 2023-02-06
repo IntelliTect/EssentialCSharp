b488091717157d45a4ad1e13a51e60097eccfa92
Mark Michaelis <mark@IntelliTect.com>
2023-02-05 16:02:42 -0800

    Updated to add Inheritance, Customization, and with Operator

--- src/Chapter09.Tests/Listing09.04.DeclaringRecordClass.Tests.cs
+++ src/Chapter09.Tests/Listing09.04.DeclaringRecordClass.Tests.cs
@@ -1,6 +1,7 @@
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_04.Tests;
 using AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_01;
-using AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Tests;
+using AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_06B;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 [TestClass]
 public partial class CoordinateTests
@@ -27,7 +28,7 @@ public partial class CoordinateTests
     {
         Coordinate coordinate1 = new(
             new Angle(180, 0, 0), new Angle(180, 0, 0));
-        GeoCoordinate coordinate2 = new(
+        NamedCoordinate coordinate2 = new(
             coordinate1.Longitude, coordinate1.Latitude, "Test");
         
 
@@ -40,5 +41,27 @@ public partial class CoordinateTests
         Coordinate coordinate = new(
             new Angle(180, 0, 0), new Angle(180, 0, 0));
         int hashCode1 = coordinate.GetHashCode();
+    }
+
+    [TestMethod]
+    public void With_Instance_Equivalent()
+    {
+        Coordinate coordinate1 = new(
+            new Angle(180, 0, 0), new Angle(180, 0, 0));
+        Coordinate coordinate2 = coordinate1 with { };
+        Assert.AreEqual(coordinate1, coordinate2);
+    }
+
+    [TestMethod]
+    public void With_DifferentInstance_NotEquivalent()
+    {
+        Coordinate coordinate1 = 
+            new(new Angle(180, 0, 0), new Angle(180, 0, 0));
+        Angle angle = new Angle(0, 0, 0);
+        Coordinate coordinate2 = coordinate1 with
+        {
+            Latitude  = angle
+        };
+        Assert.AreNotEqual(coordinate1, coordinate2);
     }
 }
--- src/Chapter09.Tests/Listing09.06C.CustomizingARecord.Tests.cs
+++ src/Chapter09.Tests/Listing09.06C.CustomizingARecord.Tests.cs
@@ -1,4 +1,4 @@
-namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_06.Tests;
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_06C.Tests;
 
 [TestClass]
 public class AngleTests
