ab6e647bd939c72c447478a516070099c2059a2f
Benjamin Michaelis <benjamin@michaelis.net>
2023-02-08 08:44:07 -0800

    Add region to 9.04 (#453)

--- src/Chapter09/Listing09.04.DeclaringRecordClass.cs
+++ src/Chapter09/Listing09.04.DeclaringRecordClass.cs
@@ -1,8 +1,10 @@
 using AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_01;
 
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_04;
-
+#region INCLUDE
+// Use the record struct construct to declare a value type
 public record class Coordinate(Angle Longitude, Angle Latitude)
+#endregion INCLUDE
 {
     public Type ExternalEqualityContract => EqualityContract;
 }
