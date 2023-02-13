fbb9e487ed622cec8b054afc353dd99d7a88b51c
Mark Michaelis <mark@IntelliTect.com>
2023-02-13 00:47:00 -0800

    Fixed infinite recursion error in Chapter 9

--- src/Chapter09.Tests/Listing09.05.EquivalentRecordClassCode.Tests.cs
+++ src/Chapter09.Tests/Listing09.05.EquivalentRecordClassCode.Tests.cs
@@ -1,7 +1,6 @@
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_05.Tests;
 using AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_01;
-using AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_04;
-using AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_07;
+using AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_05;
 
 [TestClass]
 public class CoordinateTests
@@ -18,16 +17,22 @@ public class CoordinateTests
     }
 
     [TestMethod]
-    public void EqualityContract_GetType()
+    public void Equality_WithNulls_Success()
     {
-        Coordinate coordinate1 = new(
+        Coordinate? coordinate1 = null;
+        Coordinate? coordinate2 = null;
+
+        Assert.IsTrue(coordinate1 == coordinate2);
+    }
+
+    [TestMethod]
+    public void Equality_WithLeffNull_Success()
+    {
+        Coordinate? coordinate1 = null;
+        Coordinate? coordinate2 = new(
             new Angle(180, 0, 0), new Angle(180, 0, 0));
-        NamedCoordinate geoCoordinate = new(
-            new Angle(180, 0, 0), new Angle(180, 0, 0), "GeoCoordinate");
 
-        Assert.AreEqual(coordinate1.ExternalEqualityContract, coordinate1.GetType());
-
-        Assert.AreEqual(geoCoordinate.ExternalEqualityContract, geoCoordinate.GetType());
-        Assert.AreEqual(((Coordinate)geoCoordinate).ExternalEqualityContract, ((Coordinate)geoCoordinate).GetType());
+        Assert.IsFalse(coordinate1 == coordinate2);
+        Assert.IsFalse(coordinate2 == coordinate1);
     }
 }
