6517476784224a88797fd7924384b14d1a6bfe26
Benjamin Michaelis <benjamin@michaelis.net>
2023-03-18 22:55:55 -0700

    Revert table 3.03 and 10.18

--- src/Chapter10/Listing10.18.RegisteringAFinalizerWithProcessExit.cs
+++ src/Chapter10/Listing10.18.RegisteringAFinalizerWithProcessExit.cs
@@ -1,134 +1,135 @@
-namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter10.Listing10_18;
-
-using System;
-#region INCLUDE
-using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using static ConsoleLogger;
-
-public static class Program
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter10.Listing10_18
 {
-    public static void Main(string[] args)
+    using System;
+    #region INCLUDE
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.CompilerServices;
+    using static ConsoleLogger;
+
+    public static class Program
     {
-        WriteLine("Starting...");
-        DoStuff();
-        if (args.Any(arg => arg.ToLower() == "-gc"))
+        public static void Main(string[] args)
         {
-            GC.Collect();
-            GC.WaitForPendingFinalizers();
+            WriteLine("Starting...");
+            DoStuff();
+            if (args.Any(arg => arg.ToLower() == "-gc"))
+            {
+                GC.Collect();
+                GC.WaitForPendingFinalizers();
+            }
+            WriteLine("Exiting...");
         }
-        WriteLine("Exiting...");
-    }
 
-    public static void DoStuff()
-    {
-        // ...
-        
-        WriteLine("Starting...");
-        SampleUnmanagedResource? sampleUnmanagedResource = null;
-
-        try
+        public static void DoStuff()
         {
-            sampleUnmanagedResource =
-                new SampleUnmanagedResource();
-            // Use unmanaged Resource
+            // ...
+            
+            WriteLine("Starting...");
+            SampleUnmanagedResource? sampleUnmanagedResource = null;
+
+            try
+            {
+                sampleUnmanagedResource =
+                    new SampleUnmanagedResource();
+                // Use unmanaged Resource
+                // ...
+            }
+            finally
+            {
+                if (Environment.GetCommandLineArgs().Any(
+                arg => arg.ToLower() == "-dispose"))
+                {
+                    sampleUnmanagedResource?.Dispose();
+                }
+            }
+
+            WriteLine("Exiting...");
+
             // ...
         }
-        finally
+    }
+
+    class SampleUnmanagedResource : IDisposable
+    {
+        public SampleUnmanagedResource(string fileName)
         {
-            if (Environment.GetCommandLineArgs().Any(
-            arg => arg.ToLower() == "-dispose"))
+            WriteLine("Starting...",
+                $"{nameof(SampleUnmanagedResource)}.ctor");
+
+            WriteLine("Creating managed stuff...",
+                $"{nameof(SampleUnmanagedResource)}.ctor");
+            WriteLine("Creating unmanaged stuff...",
+                $"{nameof(SampleUnmanagedResource)}.ctor");
+
+            WeakReference<IDisposable> weakReferenceToSelf =
+                 new(this);
+            ProcessExitHandler = (_, __) =>
             {
-                sampleUnmanagedResource?.Dispose();
-            }
+                WriteLine("Starting...", "ProcessExitHandler");
+                if (weakReferenceToSelf.TryGetTarget(
+                    out IDisposable? self))
+                {
+                    self.Dispose();
+                }
+                WriteLine("Exiting...", "ProcessExitHandler");
+            };
+            AppDomain.CurrentDomain.ProcessExit
+                += ProcessExitHandler;
+            WriteLine("Exiting...",
+                $"{nameof(SampleUnmanagedResource)}.ctor");
         }
 
-        WriteLine("Exiting...");
+        // Stores the process exit delegate so that we can remove it
+        // if Dispose() or Finalize() is called already.
+        private EventHandler ProcessExitHandler { get; }
 
-        // ...
-    }
-}
+        public SampleUnmanagedResource()
+            : this(Path.GetTempFileName()) { }
 
-class SampleUnmanagedResource : IDisposable
-{
-    public SampleUnmanagedResource(string fileName)
-    {
-        WriteLine("Starting...",
-            $"{nameof(SampleUnmanagedResource)}.ctor");
-
-        WriteLine("Creating managed stuff...",
-            $"{nameof(SampleUnmanagedResource)}.ctor");
-        WriteLine("Creating unmanaged stuff...",
-            $"{nameof(SampleUnmanagedResource)}.ctor");
-
-        WeakReference<IDisposable> weakReferenceToSelf =
-             new(this);
-        ProcessExitHandler = (_, __) =>
+        ~SampleUnmanagedResource()
         {
-            WriteLine("Starting...", "ProcessExitHandler");
-            if (weakReferenceToSelf.TryGetTarget(
-                out IDisposable? self))
-            {
-                self.Dispose();
-            }
-            WriteLine("Exiting...", "ProcessExitHandler");
-        };
-        AppDomain.CurrentDomain.ProcessExit
-            += ProcessExitHandler;
-        WriteLine("Exiting...",
-            $"{nameof(SampleUnmanagedResource)}.ctor");
-    }
-
-    // Stores the process exit delegate so that we can remove it
-    // if Dispose() or Finalize() is called already.
-    private EventHandler ProcessExitHandler { get; }
-
-    public SampleUnmanagedResource()
-        : this(Path.GetTempFileName()) { }
-
-    ~SampleUnmanagedResource()
-    {
-        WriteLine("Starting...");
-        Dispose(false);
-        WriteLine("Exiting...");
-    }
-
-    public void Dispose()
-    {
-        Dispose(true);
-        #region EXCLUDE
-        // Request that the finalizer not be called for this object.
-        GC.SuppressFinalize(this);
-        #endregion EXCLUDE
-    }
-
-    public void Dispose(bool disposing)
-    {
-        WriteLine("Starting...");
-
-        // Do not dispose of an owned managed object (one with a 
-        // finalizer) if called by member finalize,
-        // as the owned managed objects finalize method 
-        // will be (or has been) called by finalization queue 
-        // processing already
-        if (disposing)
-        {
-            WriteLine("Disposing managed stuff...");
+            WriteLine("Starting...");
+            Dispose(false);
+            WriteLine("Exiting...");
         }
 
-        AppDomain.CurrentDomain.ProcessExit -=
-            ProcessExitHandler;
+        public void Dispose()
+        {
+            Dispose(true);
+            #region EXCLUDE
+            // Request that the finalizer not be called for this object.
+            GC.SuppressFinalize(this);
+            #endregion EXCLUDE
+        }
 
-        WriteLine("Disposing unmanaged stuff...");
+        public void Dispose(bool disposing)
+        {
+            WriteLine("Starting...");
 
-        WriteLine("Exiting...");
+            // Do not dispose of an owned managed object (one with a 
+            // finalizer) if called by member finalize,
+            // as the owned managed objects finalize method 
+            // will be (or has been) called by finalization queue 
+            // processing already
+            if (disposing)
+            {
+                WriteLine("Disposing managed stuff...");
+            }
+
+            AppDomain.CurrentDomain.ProcessExit -=
+                ProcessExitHandler;
+
+            WriteLine("Disposing unmanaged stuff...");
+
+            WriteLine("Exiting...");
+        }
     }
-}
-#endregion INCLUDE
+    #endregion INCLUDE
 
-public static class ConsoleLogger
-{
-    public static void WriteLine(string? message = null, [CallerMemberName] string? name = null)
-        => Console.WriteLine($"{$"{name}: " }{ message ?? ": Executing" }");
+    public static class ConsoleLogger
+    {
+        public static void WriteLine(string? message = null, [CallerMemberName] string? name = null)
+            => Console.WriteLine($"{$"{name}: " }{ message ?? ": Executing" }");
+    }
 }
