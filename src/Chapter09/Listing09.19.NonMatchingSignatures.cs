09e930439f5c1b1a8e019cd027aeadc827435026
Elizabeth Pauley <121735573+elizabeth-pauley@users.noreply.github.com>
2023-08-02 13:20:17 -0700

    Update src/Chapter09/Listing09.19.NonMatchingSignatures.cs
    Co-authored-by: Kevin B <Keboo@users.noreply.github.com>

--- src/Chapter09/Listing09.19.NonMatchingSignatures.cs
+++ src/Chapter09/Listing09.19.NonMatchingSignatures.cs
@@ -2,7 +2,6 @@ using System.Runtime.CompilerServices;
 
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_19;
 
-[CompilerGenerated]
 public readonly struct Angle : IEquatable<Angle>
 {
     public int Degrees { get; init; }
