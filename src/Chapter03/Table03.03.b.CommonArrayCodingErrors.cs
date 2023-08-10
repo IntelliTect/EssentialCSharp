dc16c6d4d242bcf8964ab1403b75d7e72dcc5b32
Benjamin Michaelis <benjamin@michaelis.net>
2023-08-10 17:21:35 +0200

    Update src/Chapter03/Table03.03.b.CommonArrayCodingErrors.cs

--- src/Chapter03/Table03.03.b.CommonArrayCodingErrors.cs
+++ src/Chapter03/Table03.03.b.CommonArrayCodingErrors.cs
@@ -3,7 +3,7 @@
 
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter03.Table03_03;
 
-public partial class CommonArayCodingErrors
+public partial class CommonArrayCodingErrors
 {
     // 2.
     static public void NewKeywordWithDataTypeRequired()
