5cb74ebe8c127b0d2bb805f665ce6af06fc1a2ba
Mark Michaelis <mark@intelliTect.com>
2023-08-12 08:04:42 -0700

    Switched to use file-scoped namespace (#511)

--- src/Chapter20/Listing20.08.SignaturesOfAsyncEnumerableSelectMethods.cs
+++ src/Chapter20/Listing20.08.SignaturesOfAsyncEnumerableSelectMethods.cs
@@ -5,29 +5,27 @@ namespace System.Linq
  */
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter20.Listing20_08
 #endregion EXCLUDE
+public static class AsyncEnumerable
 {
-    public static class AsyncEnumerable
-    {
-        // ...
-        #region HIGHLIGHT
-        public static IAsyncEnumerable<TResult> Select<TSource?, TResult?>(
-        #endregion HIGHLIGHT
-      this IAsyncEnumerable<TSource> source,
-      Func<TSource, TResult> selector);
-        #region HIGHLIGHT
-        public static IAsyncEnumerable<TResult> SelectAwait<TSource?, TResult?>(
-        #endregion HIGHLIGHT
-              this IAsyncEnumerable<TSource> source,
-              Func<TSource, ValueTask<TResult>>? selector);
-        public static
-        #region HIGHLIGHT
-          IAsyncEnumerable<TResult> SelectAwaitWithCancellation<
-        #endregion HIGHLIGHT
-            TSource?, TResult?>(
-              this IAsyncEnumerable<TSource> source,
-              Func<TSource, CancellationToken, 
-                  ValueTask<TResult>> selector);
-        // ...
-    }
+    // ...
+    #region HIGHLIGHT
+    public static IAsyncEnumerable<TResult> Select<TSource?, TResult?>(
+    #endregion HIGHLIGHT
+    this IAsyncEnumerable<TSource> source,
+    Func<TSource, TResult> selector);
+    #region HIGHLIGHT
+    public static IAsyncEnumerable<TResult> SelectAwait<TSource?, TResult?>(
+    #endregion HIGHLIGHT
+            this IAsyncEnumerable<TSource> source,
+            Func<TSource, ValueTask<TResult>>? selector);
+    public static
+    #region HIGHLIGHT
+        IAsyncEnumerable<TResult> SelectAwaitWithCancellation<
+    #endregion HIGHLIGHT
+        TSource?, TResult?>(
+            this IAsyncEnumerable<TSource> source,
+            Func<TSource, CancellationToken, 
+                ValueTask<TResult>> selector);
+    // ...
 }
 #endregion INCLUDE
