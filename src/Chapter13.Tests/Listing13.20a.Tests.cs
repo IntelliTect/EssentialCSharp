cba75713412cbd64eaad3fe0de60f1f737ce26f5
Mark Michaelis <mark@IntelliTect.com>
2023-08-27 15:27:17 +0100

    Added static local function example

---
+++ src/Chapter13.Tests/Listing13.20a.Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AddisonWesley.Michaelis.EssentialCSharp.Shared.Tests;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter13.Listing13_20a.Tests;
+
+[TestClass]
+public class ProgramTests
+{
+    [TestMethod]
+    public async Task StaticAnonymousFunctionBehavior()
+    {
+        await CompilerAssert.CompileAsync(
+            new string[] { "Listing13.20a.StaticAnonymousFunctions.cs",
+                "Listing13.11.UsingADifferentFuncCompatibleMethod.cs"},
+            new string[] { "CS8820" } );
+    }
+}
