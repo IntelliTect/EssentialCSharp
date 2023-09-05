6df05789da153c81643f9b90f3c3e3ad3617c7ce
Mark Michaelis <mark@IntelliTect.com>
2023-09-05 18:10:53 +0100

    Update tables in chapter 13 and 3

---
+++ src/Chapter13/Table13.01.a.LambdaExpressionNotesAndExamples.cs
@@ -0,0 +1,17 @@
+﻿// Justification: Only snippets of source code shown for elucidation.
+#pragma warning disable CS0168 // Variable is declared but never used
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter13.Table13_01;
+
+public partial class CommonArayCodingErrors
+{
+    // 1.
+    static public void SquareBracketsOnVariableRatherThanType()
+    {
+//#if COMPILEERROR
+#if !NET6_0_OR_GREATER
+        var v = (int x) => x;
+#endif
+//#endif // COMPILEERROR
+    }
+}
