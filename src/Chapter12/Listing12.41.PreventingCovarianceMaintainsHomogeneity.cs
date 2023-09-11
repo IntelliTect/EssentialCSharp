cc041b86e44de43a2b64a7bc30e822743e907877
Daniel <dan.olvera@intellitect.com>
2023-09-11 08:57:56 -0700

    fix: Uncommented listing and added compilerAssert tests
    Had to add an extra class that derived from PdaItem. The code now throws the error

--- src/Chapter12/Listing12.41.PreventingCovarianceMaintainsHomogeneity.cs
+++ src/Chapter12/Listing12.41.PreventingCovarianceMaintainsHomogeneity.cs
@@ -1,4 +1,5 @@
-using AddisonWesley.Michaelis.EssentialCSharp.Chapter08.Listing08_05;
+using AddisonWesley.Michaelis.EssentialCSharp.Chapter08.Listing08_02;
+using AddisonWesley.Michaelis.EssentialCSharp.Chapter12.Listing12_08;
 using AddisonWesley.Michaelis.EssentialCSharp.Chapter12.Listing12_11;
 
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter12.Listing12_41;
@@ -14,12 +15,21 @@ public class Program
         Pair<Contact> contacts = new(contact1, contact2);
 
         #region HIGHLIGHT
+        #if COMPILEERROR // EXCLUDE
         // This gives an error: Cannot convert type ...
         // But suppose it did not
-        // IPair<PdaItem> pdaPair = (IPair<PdaItem>) contacts;
+        IPair<PdaItem> pdaPair = (IPair<PdaItem>) contacts;
         // This is perfectly legal, but not type-safe
-        // pair.First = new Address("123 Sesame Street");
+        pdaPair.First = new Address("123 Sesame Street");
+        #endif // COMPILEERROR // EXCLUDE
         #endregion HIGHLIGHT
         #endregion INCLUDE
+    }
+}
+
+public class Address : PdaItem
+{
+    public Address(string address) : base(address)
+    {
     }
 }
