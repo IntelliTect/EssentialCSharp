130274b5bc6a2e9d6be5178a3848a2d721f9957f
Mark Michaelis <mark@intelliTect.com>
2023-09-11 17:28:00 +0100

    Corrected comment - Chapter 9 (#544)

--- src/Chapter09/Listing09.05.DeclaringRecordClass.cs
+++ src/Chapter09/Listing09.05.DeclaringRecordClass.cs
@@ -2,7 +2,7 @@ using AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_02;
 
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_05;
 #region INCLUDE
-// Use the record struct construct to declare a value type
+// Use the record class construct to declare a reference type
 public record class Coordinate(
     Angle Longitude, Angle Latitude)
 #endregion INCLUDE
