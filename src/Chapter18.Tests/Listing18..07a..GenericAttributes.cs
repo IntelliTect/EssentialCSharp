479ec6b784ea4d17b566614db35fac89319b4c52
Mark Michaelis <mark@IntelliTect.com>
2023-09-14 02:43:01 +0100

    Added generic attribute example.

---
+++ src/Chapter18.Tests/Listing18..07a..GenericAttributes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter18.Listing18_07a.Tests;
+
+[TestClass]
+public class GenericExceptionTests
+{
+    [TestMethod]
+    public void ExpectedExceptionIsThrown()
+    {
+        ExpectedException<ArgumentNullException>.AssertExceptionThrown(
+            SampleTests.ThrowArgumentNullExceptionTest);
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
+    public void ExpectedExceptionIsNotThrown()
+    {
+        ExpectedException<ArgumentNullException>.AssertExceptionThrown(
+            () => { });
+    }
+}
