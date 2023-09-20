65ff6554621c7bbfe04eca8f1e70724194b4a32d
Mark Michaelis <mark@intelliTect.com>
2023-09-20 22:26:30 +0100

    Add code for Generic attributes and Caller* attributes - Chapter 18 (#548)

--- src/Chapter18/Listing18.31.ImplementingACustomDynamicObject.cs
+++ src/Chapter18/Listing18.31.ImplementingACustomDynamicObject.cs
@@ -1,4 +1,4 @@
-namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter18.Listing18_29;
+namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter18.Listing18_31;
 
 #region INCLUDE
 using System.Dynamic;
