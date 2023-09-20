2c3582bd26101682e3e9c12a41c21d0f5d224ccb
A-Tanner <Ray.Tanner@Intellitect.com>
2023-09-19 17:27:53 -0700

    refactor: rewrite listing tests for table 13.01

--- src/Chapter13.Tests/Table13.01.Tests.cs
+++ src/Chapter13.Tests/Table13.01.Tests.cs
@@ -12,13 +12,13 @@ public class LambdaHighlightTests
     /* 1. */ [DataRow(".a")]
     /* 2. */ [DataRow(".b")]
     /* 3. */ [DataRow(".c")]
-    /* 4. */ [DataRow(".d")]
-    /* 5. */ [DataRow(".e")]
-    /* 6. */ [DataRow(".f")]
-    /* 7. */ [DataRow(".g")]
-    /* 8. */ [DataRow(".h")]
-    /* 9. */ [DataRow(".i")]
-    /* 10. */ [DataRow(".j")]
+    /* 4. */ [DataRow(".d", "CS0023")]
+    /* 5. */ [DataRow(".e", "CS0837")]
+    /* 6. */ [DataRow(".f", "CS0029", "CS1662")]
+    /* 7. */ [DataRow(".g", "CS8070", "CS1632")]
+    /* 8. */ [DataRow(".h", "CS0103")]
+    /* 9. */ [DataRow(".i", "CS0165")]
+    /* 10. */ [DataRow(".j", "CS0165")]
     public async Task ParseAndCompile(string fileNameSuffix, params string[] errorIds)
     {
         await CompilerAssert.CompileAsync($"Table13.01{fileNameSuffix}.LambdaExpressionNotesAndExamples.cs", errorIds);
