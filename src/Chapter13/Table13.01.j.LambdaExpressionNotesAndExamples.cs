a87cadaf8b9646af25bf747d8f0e1a43cd9be601
Benjamin Michaelis <benjamin@michaelis.net>
2023-09-20 23:25:39 +0100

    Cleanup and fix

--- src/Chapter13/Table13.01.j.LambdaExpressionNotesAndExamples.cs
+++ src/Chapter13/Table13.01.j.LambdaExpressionNotesAndExamples.cs
@@ -8,16 +8,18 @@ public partial class LambdaExpressionNotesAndExamples
     // 10.
     public static void CompilerWillNotDetectInLambdaAssignment()
     {
-//#if COMPILEERROR
+        //#if COMPILEERROR
 #if !NET6_0_OR_GREATER
-    Func<int, bool> isFortyTwo =
-        x => 42 == x;
-    if(isFortyTwo(42))
-    {
-      //ERROR: Use of unassigned local variable
-        System.Console.Write(number);
-    }
+
+        int number;
+        Func<int, bool> isFortyTwo =
+          x => 42 == (number = x);
+        if (isFortyTwo(42))
+        {
+            // ERROR: Use of unassigned local variable
+            System.Console.Write(number);
+        }
 #endif
-//#endif // COMPILEERROR
+        //#endif // COMPILEERROR
     }
 }
