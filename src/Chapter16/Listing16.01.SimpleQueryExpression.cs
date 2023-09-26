2f3920f23805f0a5414be213a139491d298e0916
Mark Michaelis <mark@IntelliTect.com>
2023-09-26 10:16:01 +0000

    Reduced line lengths.

--- src/Chapter16/Listing16.01.SimpleQueryExpression.cs
+++ src/Chapter16/Listing16.01.SimpleQueryExpression.cs
@@ -6,28 +6,32 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter16.Listing16
     #region INCLUDE
     public static class CSharp
     {
-        public static readonly ReadOnlyCollection<string> Keywords = new(
-            new string[]
-            {
-                "abstract","add*","alias*","as","ascending*",
-                "async*","await*","base","bool","break",
-                "by*","byte","case","catch","char","checked",
-                "class","const","continue","decimal","default",
-                "delegate","descending*","do","double","dynamic*",
-                "else","enum","equals*","event","explicit","extern",
-                "false","finally","fixed","float","for","foreach",
-                "from*","get*","global*","goto","group*","if","implicit",
-                "in","int","interface","internal","into*","is","join*",
-                "let*","lock","long", "nameof*", "namespace","new",
-                "nonnull*","null","object","on*","operator","orderby*",
-                "out","override","params","partial*","private",
-                "protected","public","readonly","ref","remove*","return",
-                "sbyte","sealed","select*","set*","short","sizeof",
-                "stackalloc","static","string","struct","switch","this",
-                "throw","true","try","typeof","uint","ulong","unchecked",
-                "unsafe","ushort","using","value*","var*","virtual",
-                "void","volatile","when*","where*","while","yield*"
-            });
+        public static readonly ReadOnlyCollection<string> Keywords = 
+            new(
+                new string[]
+                {
+                    "abstract","add*","alias*","as","ascending*",
+                    "async*","await*","base","bool","break",
+                    "by*","byte","case","catch","char","checked",
+                    "class","const","continue","decimal","default",
+                    "delegate","descending*","do","double","dynamic*",
+                    "else","enum","equals*","event","explicit",
+                    "extern","false","finally","fixed","float","for",
+                    "foreach","from*","get*","global*","goto","group*",
+                    "if","implicit","in","int","interface","internal",
+                    "into*","is","join*","let*","lock","long",
+                    "nameof*", "namespace","new","nonnull*","null",
+                    "object","on*","operator","orderby*","out",
+                    "override","params","partial*","private",
+                    "protected","public","readonly","ref","remove*",
+                    "return","sbyte","sealed","select*","set*","short",
+                    "sizeof","stackalloc","static","string","struct",
+                    "switch","this","throw","true","try","typeof",
+                    "uint","ulong","unchecked","unsafe","ushort",
+                    "using","value*","var*","virtual","void","volatile",
+                    "when*","where*","while","yield*"
+                }
+            );
     }
     #region EXCLUDE
 }
