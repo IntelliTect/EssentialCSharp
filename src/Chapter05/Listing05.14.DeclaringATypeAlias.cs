622ef1fe8650fb1ae93779fdb88e15abc4aa8992
Mark Michaelis <mark@intelliTect.com>
2023-09-27 21:09:12 +0000

    Modified listing to include aliasing any type example (#558)

--- src/Chapter05/Listing05.14.DeclaringATypeAlias.cs
+++ src/Chapter05/Listing05.14.DeclaringATypeAlias.cs
@@ -4,6 +4,9 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter05.Listing05_14;
 #region INCLUDE
 #region HIGHLIGHT
 using CountDownTimer = System.Timers.Timer;
+#if NET8_0_OR_GREATER // EXCLUDE
+using StartStop = (DateTime Start, DateTime Stop);
+#endif // NET8_0_OR_GREATER // EXCLUDE
 #endregion HIGHLIGHT
 
 public class HelloWorld
@@ -12,6 +15,9 @@ public class HelloWorld
     {
         #region HIGHLIGHT
         CountDownTimer timer;
+        #if NET8_0_OR_GREATER // EXCLUDE
+        StartStop startStop;
+        #endif // NET8_0_OR_GREATER // EXCLUDE
         #endregion HIGHLIGHT
 
         // ...
