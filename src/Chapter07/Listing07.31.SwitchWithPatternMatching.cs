b35c1825a1092fdf22128907fcf74294dbdbf72b
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2023-11-06 18:10:01 +0100

    Revert file static order

--- src/Chapter07/Listing07.31.SwitchWithPatternMatching.cs
+++ src/Chapter07/Listing07.31.SwitchWithPatternMatching.cs
@@ -27,7 +27,7 @@ public class Program
             compositeFormatString, date.Year, date.Month, date.Day) : null;
     #endregion INCLUDE
 }
-static file class DateDeconstructors
+file static class DateDeconstructors
 {
     public static void Deconstruct(this DateTime date, out int year, out int month, out int day) => (year, month, day) = (date.Year, date.Month, date.Day);
 }
