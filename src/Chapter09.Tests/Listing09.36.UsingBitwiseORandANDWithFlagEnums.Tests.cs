1b2f1aa06ffcb68ba56b3d4d8afed7e64cc133d2
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2023-11-08 01:07:52 +0100

    feat: Update listing 9.36 to include linux (#588)
    Fixes #117
    
    A file name that starts with a `.` on linux is considered hidden.

--- src/Chapter09.Tests/Listing09.36.UsingBitwiseORandANDWithFlagEnums.Tests.cs
+++ src/Chapter09.Tests/Listing09.36.UsingBitwiseORandANDWithFlagEnums.Tests.cs
@@ -1,30 +1,19 @@
-using System.Runtime.InteropServices;
-
 namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter09.Listing09_36.Tests;
 
 [TestClass]
 public class ProgramTests
 {
     [TestMethod]
-    public void Main_ExpectHiddentAndReadOnlyFlags()
+    public void Main_ExpectHiddenAndReadOnlyFlags()
     {
         Directory.SetCurrentDirectory(AppContext.BaseDirectory);
         FileAttributes fileAttributes;
         string expected;
 
-        if (System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-        {
-            // The only working file attribute on Linux is ReadOnly.
-            fileAttributes = FileAttributes.ReadOnly;
-            Assert.AreEqual<int>(1, (int)fileAttributes);
-            expected = $@"ReadOnly = {(int)fileAttributes}";
-        }
-        else
-        {
-            fileAttributes = FileAttributes.Hidden | FileAttributes.ReadOnly;
-            Assert.AreEqual<int>(3, (int)fileAttributes);
-            expected = $@"ReadOnly, Hidden = {(int)fileAttributes}";
-        }
+        fileAttributes = FileAttributes.Hidden | FileAttributes.ReadOnly;
+        Assert.AreEqual<int>(3, (int)fileAttributes);
+        expected = $@"ReadOnly, Hidden = {(int)fileAttributes}";
+
         
         IntelliTect.TestTools.Console.ConsoleAssert.Expect(
             expected, Program.Main);
