9e0bd5a80a3a5b615fa030a8d1d904c8cd87040a
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2023-11-11 08:47:54 +0100

    feat: Test Listing 20.7
    Fixes #347

--- src/Chapter20.Tests/Listing20.07.Tests.cs
+++ src/Chapter20.Tests/Listing20.07.Tests.cs
@@ -1,14 +1,12 @@
-﻿//using System.Threading.Tasks;
-
-//namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter20.Listing20_07.Tests
-//{
-//    [TestClass]
-//    public class ProgramTests
-//    {
-//        [TestMethod]
-//        public async Task MainTest()
-//        {
-//            await IntelliTect.TestTools.Console.ConsoleAssert.ExpectAsync("<filename>", async () => await AsyncEncryptionCollection.Main());
-//        }
-//    }
-//}
+﻿namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter20.Listing20_07.Tests
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public async Task MainTest()
+        {
+            await IntelliTect.TestTools.Console.ConsoleAssert.ExecuteAsync("", async () => await AsyncEncryptionCollection.Main());
+        }
+    }
+}
