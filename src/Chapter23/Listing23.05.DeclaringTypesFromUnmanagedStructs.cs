9b9fb22854c337a2d7fa7521082a6ef2487ff83a
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2024-01-23 17:33:31 -0800

    fix: Fix pragma being cutoff (#685)

--- src/Chapter23/Listing23.05.DeclaringTypesFromUnmanagedStructs.cs
+++ src/Chapter23/Listing23.05.DeclaringTypesFromUnmanagedStructs.cs
@@ -12,9 +12,9 @@ namespace AddisonWesley.Michaelis.EssentialCSharp.Chapter23.Listing23_05
         public byte Green;
         public byte Blue;
         // Turn off the warning about not accessing Unused
-#pragma warning disable 414
+    #pragma warning disable 414
         private byte Unused;
-#pragma warning restore 414
+    #pragma warning restore 414
 
         public ColorRef(byte red, byte green, byte blue)
         {
