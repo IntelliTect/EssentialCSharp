dd98f12fa273df70dc02506ac4b8bc625ae4d8ee
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2025-03-04 19:18:16 -0800

    fix: Add line numbers to Listing 4.62 (#809)
    Fixes #623

--- src/Chapter04/Listing04.62.LineWarning.cs
+++ src/Chapter04/Listing04.62.LineWarning.cs
@@ -4,10 +4,16 @@ public class Program
 {
     public static void Main()
     {
-        #region INCLUDE
         #line 113 "TicTacToe.cs"
         #warning "Same move allowed multiple times."
         #line default
+        // Duplicating content to add line numbers
+        /*
+        #region INCLUDE
+        124.        #line 113 "TicTacToe.cs"
+        125.        #warning "Same move allowed multiple times."
+        126.        #line default
         #endregion INCLUDE
+        */
     }
 }
