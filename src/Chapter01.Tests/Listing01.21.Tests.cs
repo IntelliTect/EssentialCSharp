0cedd893fc60ec9768df91eab823c54719c47f1b
Benjamin Michaelis <git@relay.benjamin.michaelis.net>
2025-04-08 17:28:15 -0700

    fix: Adjust wording for 1.21 instead of output

--- src/Chapter01.Tests/Listing01.21.Tests.cs
+++ src/Chapter01.Tests/Listing01.21.Tests.cs
@@ -11,7 +11,7 @@ public class ProgramTests
 @"Hey you!
 Enter your first name: <<Inigo
 >>Enter your last name: <<Montoya
->>Your full name is Montoya, Inigo.";
+>>Your full name is Inigo Montoya.";
 
         IntelliTect.TestTools.Console.ConsoleAssert.Expect(
             expected, CommentSamples.Main);
